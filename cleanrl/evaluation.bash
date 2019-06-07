aac70ad94f03c38a04440120174f137b0cd4e500
Costa Huang <costa.huang@outlook.com>
2019-06-07 15:48:05 -0400

    Master # Update gitignore to ignore tensorflow files

--- cleanrl/evaluation.bash
+++ cleanrl/evaluation.bash
@@ -2,7 +2,7 @@
 # evaluate the algorithm in parallel
 NUM_CORES=2
 export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
-for seed in {1..2}
+for seed in {1..10}
 do
     (sleep 0.3 && nohup python a2c.py --seed $seed) >& /dev/null &
 done
