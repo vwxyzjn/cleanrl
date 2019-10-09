713fa58eef4d680b2ee449731f81f1786fd32029
Costa Huang <costa.huang@outlook.com>
2019-10-09 16:44:49 +0000

    Master # Add torch dependency

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='cleanrl',
       version='0.1.0',
-      install_requires=['gym', 'tensorboardX'],
+      install_requires=['gym', 'tensorboardX', 'torch'],
       packages=find_packages()
 )
