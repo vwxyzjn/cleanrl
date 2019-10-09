c00a80c7b887f97370f7171d3c79c0bd4603c199
Costa Huang <costa.huang@outlook.com>
2019-10-09 12:18:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='cleanrl',
       version='0.1.0',
-      install_requires=['gym', 'stable_baselines'],
+      install_requires=['gym', 'stable_baselines', 'tensorboardX'],
       packages=find_packages()
 )
