dd767bc94e1ba838b7e42687ae39310a3a08e602
Costa Huang <costa.huang@outlook.com>
2019-10-09 12:18:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cleanrl',
-      version='0.0.1',
-      install_requires=['gym'],
+      version='0.1.0',
+      install_requires=['gym', 'stable_baselines'],
       packages=find_packages()
 )
