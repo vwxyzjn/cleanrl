f53024cad69d8738fcd9fc83b96a7c51d52f0296
Costa Huang <costa.huang@outlook.com>
2019-10-16 20:51:58 +0000

    Master # Update config

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='cleanrl',
       version='0.1.0',
-      install_requires=['gym', 'tensorboardX', 'torch', 'tensorboard'],
+      install_requires=['gym', 'torch', 'tensorboard', 'wandb'],
       packages=find_packages()
 )
