01d66036ee874c7cfd4393636f4a58327dc89b52
dosssman <dosssman@hotmail.fr>
2020-01-08 08:52:09 +0900

    Better fix for dimension mismatch.

--- cleanrl/experiments/sac_disc.py
+++ cleanrl/experiments/sac_disc.py
@@ -194,9 +194,9 @@ class ReplayBuffer():
             s, a, r, s_prime, done_mask = transition
             s_lst.append(s)
             a_lst.append([a])
-            r_lst.append([r])
+            r_lst.append(r)
             s_prime_lst.append(s_prime)
-            done_mask_lst.append([done_mask])
+            done_mask_lst.append(done_mask)
 
         return s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst
 
@@ -318,9 +318,6 @@ while global_step < args.total_timesteps:
             # TODO: Cast action batch to longs ?
             observation_batch, action_batch, reward_batch, \
                 next_observation_batch, terminals_batch = buffer.sample(args.batch_size)
-
-            terminals_batch = np.squeeze( terminals_batch)
-            reward_batch = np.squeeze( reward_batch)
 
             with torch.no_grad():
                 next_action_probs, next_logprobs = pg.get_action_probs(next_observation_batch)
