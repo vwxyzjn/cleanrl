209a0cd45d5cd72d2565264a8dfdb639270c78b9
Costa Huang <costa.huang@outlook.com>
2020-01-09 17:05:01 -0500

    Master # bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cleanrl',
-      version='0.1.0',
+      version='0.2.0',
       install_requires=['gym', 'torch', 'tensorboard', 'wandb'],
       packages=find_packages()
 )
