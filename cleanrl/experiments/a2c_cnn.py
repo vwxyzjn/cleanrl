cffcd8448b315c3d5a27dd87ec273ad30817b50b
Costa Huang <costa.huang@outlook.com>
2020-02-19 20:04:53 -0500

    A2C # experiment with CNN

---
+++ cleanrl/experiments/a2c_cnn.py
@@ -0,0 +1,328 @@
+import cv2
+import gym
+import gym.spaces
+import numpy as np
+import collections
+
+
+class FireResetEnv(gym.Wrapper):
+    def __init__(self, env=None):
+        """For environments where the user need to press FIRE for the game to start."""
+        super(FireResetEnv, self).__init__(env)
+        assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
+        assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def step(self, action):
+        return self.env.step(action)
+
+    def reset(self):
+        self.env.reset()
+        obs, _, done, _ = self.env.step(1)
+        if done:
+            self.env.reset()
+        obs, _, done, _ = self.env.step(2)
+        if done:
+            self.env.reset()
+        return obs
+
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env=None, skip=4):
+        """Return only every `skip`-th frame"""
+        super(MaxAndSkipEnv, self).__init__(env)
+        # most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = collections.deque(maxlen=2)
+        self._skip = skip
+
+    def step(self, action):
+        total_reward = 0.0
+        done = None
+        for _ in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            self._obs_buffer.append(obs)
+            total_reward += reward
+            if done:
+                break
+        max_frame = np.max(np.stack(self._obs_buffer), axis=0)
+        return max_frame, total_reward, done, info
+
+    def reset(self):
+        """Clear past frame buffer and init. to first obs. from inner env."""
+        self._obs_buffer.clear()
+        obs = self.env.reset()
+        self._obs_buffer.append(obs)
+        return obs
+
+
+class ProcessFrame84(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(ProcessFrame84, self).__init__(env)
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(84, 84, 1), dtype=np.uint8)
+
+    def observation(self, obs):
+        return ProcessFrame84.process(obs)
+
+    @staticmethod
+    def process(frame):
+        if frame.size == 210 * 160 * 3:
+            img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
+        elif frame.size == 250 * 160 * 3:
+            img = np.reshape(frame, [250, 160, 3]).astype(np.float32)
+        else:
+            assert False, "Unknown resolution."
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
+        x_t = resized_screen[18:102, :]
+        x_t = np.reshape(x_t, [84, 84, 1])
+        return x_t.astype(np.uint8)
+
+
+class ImageToPyTorch(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(ImageToPyTorch, self).__init__(env)
+        old_shape = self.observation_space.shape
+        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(old_shape[-1], old_shape[0], old_shape[1]),
+                                                dtype=np.float32)
+
+    def observation(self, observation):
+        return np.moveaxis(observation, 2, 0)
+
+
+class ScaledFloatFrame(gym.ObservationWrapper):
+    def observation(self, obs):
+        return np.array(obs).astype(np.float32) / 255.0
+
+
+class BufferWrapper(gym.ObservationWrapper):
+    def __init__(self, env, n_steps, dtype=np.float32):
+        super(BufferWrapper, self).__init__(env)
+        self.dtype = dtype
+        old_space = env.observation_space
+        self.observation_space = gym.spaces.Box(old_space.low.repeat(n_steps, axis=0),
+                                                old_space.high.repeat(n_steps, axis=0), dtype=dtype)
+
+    def reset(self):
+        self.buffer = np.zeros_like(self.observation_space.low, dtype=self.dtype)
+        return self.observation(self.env.reset())
+
+    def observation(self, observation):
+        self.buffer[:-1] = self.buffer[1:]
+        self.buffer[-1] = observation
+        return self.buffer
+
+
+def make_env(env_name):
+    env = gym.make(env_name)
+    env = MaxAndSkipEnv(env)
+    env = FireResetEnv(env)
+    env = ProcessFrame84(env)
+    env = ImageToPyTorch(env)
+    env = BufferWrapper(env, 4)
+    return ScaledFloatFrame(env)
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+from torch.distributions.normal import Normal
+from torch.utils.tensorboard import SummaryWriter
+
+from cleanrl.common import preprocess_obs_space, preprocess_ac_space
+import argparse
+import numpy as np
+import gym
+from gym.wrappers import TimeLimit, Monitor, AtariPreprocessing
+from gym.spaces import Discrete, Box, MultiBinary, MultiDiscrete, Space
+import time
+import random
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A2C agent')
+    # Common arguments
+    parser.add_argument('--exp-name', type=str, default=os.path.basename(__file__).rstrip(".py"),
+                       help='the name of this experiment')
+    parser.add_argument('--gym-id', type=str, default="PongNoFrameskip-v4",
+                       help='the id of the gym environment')
+    parser.add_argument('--learning-rate', type=float, default=7e-4,
+                       help='the learning rate of the optimizer')
+    parser.add_argument('--seed', type=int, default=1,
+                       help='seed of the experiment')
+    parser.add_argument('--episode-length', type=int, default=400000,
+                       help='the maximum length of each episode')
+    parser.add_argument('--total-timesteps', type=int, default=4000000,
+                       help='total timesteps of the experiments')
+    parser.add_argument('--torch-deterministic', type=bool, default=True,
+                       help='whether to set `torch.backends.cudnn.deterministic=True`')
+    parser.add_argument('--cuda', type=bool, default=True,
+                       help='whether to use CUDA whenever possible')
+    parser.add_argument('--prod-mode', type=bool, default=False,
+                       help='run the script in production mode and use wandb to log outputs')
+    parser.add_argument('--capture-video', type=bool, default=False,
+                       help='weather to capture videos of the agent performances (check out `videos` folder)')
+    parser.add_argument('--wandb-project-name', type=str, default="cleanRL",
+                       help="the wandb's project name")
+    parser.add_argument('--wandb-entity', type=str, default=None,
+                       help="the entity (team) of wandb's project")
+
+    # Algorithm specific arguments
+    parser.add_argument('--gamma', type=float, default=0.99,
+                       help='the discount factor gamma')
+    parser.add_argument('--vf-coef', type=float, default=0.25,
+                       help="value function's coefficient the loss function")
+    parser.add_argument('--max-grad-norm', type=float, default=0.5,
+                       help='the maximum norm for the gradient clipping')
+    parser.add_argument('--ent-coef', type=float, default=0.01,
+                       help="policy entropy's coefficient the loss function")
+    args = parser.parse_args()
+    if not args.seed:
+        args.seed = int(time.time())
+
+# TRY NOT TO MODIFY: setup the environment
+experiment_name = f"{args.gym_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+writer = SummaryWriter(f"runs/{experiment_name}")
+writer.add_text('hyperparameters', "|param|value|\n|-|-|\n%s" % (
+        '\n'.join([f"|{key}|{value}|" for key, value in vars(args).items()])))
+if args.prod_mode:
+    import wandb
+    wandb.init(project=args.wandb_project_name, entity=args.wandb_entity, tensorboard=True, config=vars(args), name=experiment_name, monitor_gym=True)
+    writer = SummaryWriter(f"/tmp/{experiment_name}")
+    wandb.save(os.path.abspath(__file__))
+
+# TRY NOT TO MODIFY: seeding
+device = torch.device('cuda' if torch.cuda.is_available() and args.cuda else 'cpu')
+env = make_env(args.gym_id)
+args.episode_length = 40000
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.backends.cudnn.deterministic = args.torch_deterministic
+env.seed(args.seed)
+env.action_space.seed(args.seed)
+env.observation_space.seed(args.seed)
+input_shape, preprocess_obs_fn = preprocess_obs_space(env.observation_space, device)
+output_shape = preprocess_ac_space(env.action_space)
+# respect the default timelimit
+env = TimeLimit(env, args.episode_length)	
+if args.capture_video:	
+    env = Monitor(env, f'videos/{experiment_name}')	
+assert isinstance(env.action_space, Discrete), "only discrete action space is supported"
+
+# ALGO LOGIC: initialize agent here:
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(env.observation_space.shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+        conv_out_size = self._get_conv_out(env.observation_space.shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, env.action_space.n)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        x = torch.Tensor(x).to(device)
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
+
+class Value(nn.Module):
+    def __init__(self):
+        super(Value, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(env.observation_space.shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+        conv_out_size = self._get_conv_out(env.observation_space.shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        x = torch.Tensor(x).to(device)
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
+
+pg = Policy().to(device)
+vf = Value().to(device)
+optimizer = optim.Adam(list(pg.parameters()) + list(vf.parameters()), lr=args.learning_rate)
+loss_fn = nn.MSELoss()
+
+# TRY NOT TO MODIFY: start the game
+global_step = 0
+while global_step < args.total_timesteps:
+    next_obs = np.array(env.reset())
+    actions = np.empty((args.episode_length,), dtype=object)
+    rewards, dones = np.zeros((2, args.episode_length))
+    obs = np.empty((args.episode_length,) + env.observation_space.shape)
+
+    # ALGO LOGIC: put other storage logic here
+    values = torch.zeros((args.episode_length), device=device)
+    neglogprobs = torch.zeros((args.episode_length,), device=device)
+    entropys = torch.zeros((args.episode_length,), device=device)
+
+    # TRY NOT TO MODIFY: prepare the execution of the game.
+    for step in range(args.episode_length):
+        global_step += 1
+        obs[step] = next_obs.copy()
+
+        # ALGO LOGIC: put action logic here
+        logits = pg.forward(obs[step:step+1])
+        values[step] = vf.forward(obs[step:step+1])
+
+        # ALGO LOGIC: `env.action_space` specific logic
+        probs = Categorical(logits=logits)
+        action = probs.sample()
+        actions[step], neglogprobs[step], entropys[step] = action.tolist()[0], -probs.log_prob(action), probs.entropy()
+
+        # TRY NOT TO MODIFY: execute the game and log data.
+        next_obs, rewards[step], dones[step], _ = env.step(actions[step])
+        next_obs = np.array(next_obs)
+        if dones[step]:
+            break
+
+    # ALGO LOGIC: training.
+    # calculate the discounted rewards, or namely, returns
+    returns = np.zeros_like(rewards)
+    for t in reversed(range(rewards.shape[0]-1)):
+        returns[t] = rewards[t] + args.gamma * returns[t+1] * (1-dones[t])
+    # advantages are returns - baseline, value estimates in our case
+    advantages = returns - values.detach().cpu().numpy()
+
+    vf_loss = loss_fn(torch.Tensor(returns).to(device), values) * args.vf_coef
+    pg_loss = torch.Tensor(advantages).to(device) * neglogprobs
+    loss = (pg_loss - (torch.exp(-neglogprobs) * -neglogprobs) * args.ent_coef).mean() + vf_loss
+
+    optimizer.zero_grad()
+    loss.backward()
+    nn.utils.clip_grad_norm_(list(pg.parameters()) + list(vf.parameters()), args.max_grad_norm)
+    optimizer.step()
+
+    # TRY NOT TO MODIFY: record rewards for plotting purposes
+    writer.add_scalar("charts/episode_reward", rewards.sum(), global_step)
+    writer.add_scalar("losses/value_loss", vf_loss.item(), global_step)
+    writer.add_scalar("losses/entropy", entropys[:step].mean().item(), global_step)
+    writer.add_scalar("losses/policy_loss", pg_loss.mean().item(), global_step)
+env.close()
+writer.close()
