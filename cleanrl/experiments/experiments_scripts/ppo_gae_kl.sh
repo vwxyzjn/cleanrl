69c93a5d63b751ac1beb3fba299a3b4c5fb56797
Costa Huang <costa.huang@outlook.com>
2020-02-25 15:28:48 -0500

    #10  new scripts

--- cleanrl/experiments/experiments_scripts/ppo_gae_kl.sh
+++ cleanrl/experiments/experiments_scripts/ppo_gae_kl.sh
@@ -1,4 +1,4 @@
-# This script studies the effectiveness of GAE
+# This script studies the effectiveness of GAE with KL bound
 
 NUM_CORES=2
 export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
@@ -7,8 +7,9 @@ do
     (sleep 0.3 && nohup xvfb-run -a python ppo2_continuous_action.py \
     --gym-id HopperBulletEnv-v0 \
     --total-timesteps 2000000 \
+    --kl \
     --gae \
-    --wandb-project-name cleanrl.ppo.gae \
+    --wandb-project-name cleanrl.ppo.gae.kl \
     --wandb-entity cleanrl \
     --prod-mode \
     --capture-video \
@@ -20,8 +21,9 @@ do
     (sleep 0.3 && nohup xvfb-run -a python ppo2_continuous_action.py \
     --gym-id Walker2DBulletEnv-v0 \
     --total-timesteps 2000000 \
+    --kl \
     --gae \
-    --wandb-project-name cleanrl.ppo.gae \
+    --wandb-project-name cleanrl.ppo.gae.kl \
     --wandb-entity cleanrl \
     --prod-mode \
     --capture-video \
@@ -33,8 +35,9 @@ do
     (sleep 0.3 && nohup xvfb-run -a python ppo2_continuous_action.py \
     --gym-id HumanoidBulletEnv-v0 \
     --total-timesteps 2000000 \
+    --kl \
     --gae \
-    --wandb-project-name cleanrl.ppo.gae \
+    --wandb-project-name cleanrl.ppo.gae.kl \
     --wandb-entity cleanrl \
     --prod-mode \
     --capture-video \
@@ -47,7 +50,7 @@ do
     (sleep 0.3 && nohup xvfb-run -a python ppo2_continuous_action.py \
     --gym-id HopperBulletEnv-v0 \
     --total-timesteps 2000000 \
-    --wandb-project-name cleanrl.ppo.gae \
+    --wandb-project-name cleanrl.ppo.gae.kl \
     --wandb-entity cleanrl \
     --prod-mode \
     --capture-video \
@@ -59,7 +62,7 @@ do
     (sleep 0.3 && nohup xvfb-run -a python ppo2_continuous_action.py \
     --gym-id Walker2DBulletEnv-v0 \
     --total-timesteps 2000000 \
-    --wandb-project-name cleanrl.ppo.gae \
+    --wandb-project-name cleanrl.ppo.gae.kl \
     --wandb-entity cleanrl \
     --prod-mode \
     --capture-video \
@@ -71,7 +74,7 @@ do
     (sleep 0.3 && nohup xvfb-run -a python ppo2_continuous_action.py \
     --gym-id HumanoidBulletEnv-v0 \
     --total-timesteps 2000000 \
-    --wandb-project-name cleanrl.ppo.gae \
+    --wandb-project-name cleanrl.ppo.gae.kl \
     --wandb-entity cleanrl \
     --prod-mode \
     --capture-video \
