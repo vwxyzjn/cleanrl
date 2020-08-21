b35accef3fca1bd3e24136318429859b78901185
Costa Huang <costa.huang@outlook.com>
2020-08-20 21:21:29 -0400

    push docker related files

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,3 @@ VERSION=1.4-cuda10.1-cudnn7-runtime-cleanrl-0.3.0
 docker build -t vwxyzjn/cleanrl:$VERSION  -t vwxyzjn/cleanrl:latest -f Dockerfile .
 docker push vwxyzjn/cleanrl:latest
 docker push vwxyzjn/cleanrl:$VERSION
-
-docker build -t vwxyzjn/cleanrl:m$VERSION  -t vwxyzjn/cleanrl:mlatest -f m.Dockerfile .
-docker push vwxyzjn/cleanrl:mlatest
-docker push vwxyzjn/cleanrl:m$VERSION
