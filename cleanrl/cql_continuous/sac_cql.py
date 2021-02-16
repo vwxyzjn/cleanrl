a898b022bcc72b34e9b4e57e12cc9a93e949b638
dosssman <dosssman@hotmail.fr>
2021-02-16 17:33:13 +0900

    updated sac_cql following PR comments; tested

--- cleanrl/cql_continuous/sac_cql.py
+++ cleanrl/cql_continuous/sac_cql.py
@@ -12,7 +12,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal, Uniform
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataset import IterableDataset
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 import gym
@@ -30,6 +31,8 @@ if __name__ == "__main__":
                         help='the id of the gym environment')
     parser.add_argument('--seed', type=int, default=2,
                         help='seed of the experiment')
+    parser.add_argument('--total-timesteps', type=int, default=10000000,
+                        help='total timesteps of the experiments')
     parser.add_argument('--torch-deterministic', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
                         help='if toggled, `torch.backends.cudnn.deterministic=False`')
     parser.add_argument('--cuda', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
@@ -42,10 +45,8 @@ if __name__ == "__main__":
                         help="the wandb's project name")
     parser.add_argument('--wandb-entity', type=str, default=None,
                         help="the entity (team) of wandb's project")
-    parser.add_argument('--n-epochs', type=int, default=1000,
-                        help='number of epochs (total training iters = n-epochs * epoch-length')
-    parser.add_argument('--epoch-length', type=int, default=1000,
-                        help='number of training steps in an epoch')
+    parser.add_argument('--eval-frequency', type=int, default=1000,
+                        help='after how many gradient step is the model evaluated')
     
     # Algorithm specific arguments
     parser.add_argument('--gamma', type=float, default=0.99,
@@ -94,6 +95,8 @@ if __name__ == "__main__":
                         help='Lagrange threshold for automatic tuning of the alpha_prime component')
     parser.add_argument('--with-lagrange', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
                         help='automatic tuning of the alpha_prime coefficient (min-q-weight) coefficient.')
+    parser.add_argument('--n-eval-episodes', type=int, default=5,
+                        help='How many episodes the policy is evaluated over during the test phase.')
     
     args = parser.parse_args()
     if not args.seed:
@@ -127,9 +130,6 @@ input_shape = env.observation_space.shape[0]
 output_shape = env.action_space.shape[0]
 # respect the default timelimit
 assert isinstance(env.action_space, Box), "only continuous action space is supported"
-if args.capture_video:
-    env = Monitor(env, f'videos/{experiment_name}')
-
 # ALGO LOGIC: initialize agent here:
 LOG_STD_MAX = 2
 LOG_STD_MIN = -5
@@ -197,20 +197,20 @@ class SoftQNetwork(nn.Module):
         return x
 
 # Offline RL data loading
-class ExperienceReplayDataset(Dataset):
+class ExperienceReplayDataset(IterableDataset):
     def __init__(self, env_name):
         self.dataset_env = gym.make(env_name)
         self.dataset = self.dataset_env.get_dataset()
-    def __len__(self):
-        return self.dataset['observations'].shape[0]-1
-    def __getitem__(self, index):
-        return self.dataset['observations'][:-1][index].astype(np.float32), \
-               self.dataset['actions'][:-1][index].astype(np.float32), \
-               self.dataset['rewards'][:-1][index].astype(np.float32), \
-               self.dataset['observations'][1:][index].astype(np.float32), \
-               self.dataset['terminals'][:-1][index].astype(np.float32)
+    def __iter__(self):
+        while True:
+            idx = np.random.choice(len(self.dataset['observations'])-1)
+            yield self.dataset['observations'][:-1][idx].astype(np.float32), \
+                self.dataset['actions'][:-1][idx].astype(np.float32), \
+                self.dataset['rewards'][:-1][idx].astype(np.float32), \
+                self.dataset['observations'][1:][idx].astype(np.float32), \
+                self.dataset['terminals'][:-1][idx].astype(np.float32)
 
-data_loader = DataLoader(ExperienceReplayDataset(args.offline_gym_id), batch_size=args.batch_size, num_workers=2)
+data_loader = iter(DataLoader(ExperienceReplayDataset(args.offline_gym_id), batch_size=args.batch_size, num_workers=2))
 
 pg = Policy(input_shape, output_shape, env).to(device)
 qf1 = SoftQNetwork(input_shape, output_shape, layer_init).to(device)
@@ -237,160 +237,136 @@ if args.with_lagrange:
     log_alpha_prime = torch.zeros([1], requires_grad=True, device=device)
     alpha_prime_optimizer = optim.Adam([log_alpha_prime], args.alpha_lr)
 
-# Helper to evaulate the agent, could use some clean up.
-def test_agent(env, policy, n_eval_episodes=5):
-    returns, lengths = [], []
-
-    for _ in range(n_eval_episodes):
-        ret, done, t = 0., False, 0
-        obs = np.array( env.reset())
-
-        while not done:
-            # MaxEntRL Paper argues eval should not be determinsitic
-            with torch.no_grad():
-                action, _, _ = policy.get_action(torch.Tensor(obs).unsqueeze(0).to(device))
-                action = action.tolist()[0]
-
-            obs, rew, done, _ = env.step( action)
-            obs = np.array( obs)
-            ret += rew
-            t += 1
-
-        returns.append(ret)
-        lengths.append(t)
-
-    eval_stats = {
-        "test_mean_return": np.mean(returns),
-        "test_mean_length": np.mean(lengths),
-        "test_max_return": np.max(returns),
-        "test_max_length": np.max(lengths),
-        "test_min_return": np.min(returns),
-        "test_min_length": np.min(lengths)
-    }
-
-    return eval_stats
-
 # Training Loop
-global_step = 0 # Tracks the Gradient updates
-for epoch in range(args.n_epochs):
-    for k, train_batch in enumerate(data_loader):
-        global_step += 1
-        # tenosrize, place and the correct device
-        s_obs, s_actions, s_rewards, s_next_obses, s_dones = [i.to(device) for i in train_batch]
-        
-        # Standard Bellman update component
-        with torch.no_grad():
-            next_state_actions, next_state_log_pi, _ = pg.get_action(s_next_obses)
-            qf1_next_target = qf1_target.forward(s_next_obses, next_state_actions)
-            qf2_next_target = qf2_target.forward(s_next_obses, next_state_actions)
-            min_qf_next_target = torch.min(qf1_next_target, qf2_next_target) - alpha * next_state_log_pi
-            next_q_value = s_rewards + (1 - s_dones) * args.gamma * min_qf_next_target.view(-1)
+for global_step in range(args.total_timesteps):
+    # sample a batch from the dataset and put on the appropriate device
+    s_obs, s_actions, s_rewards, s_next_obses, s_dones = [i.to(device) for i in next(data_loader)]
+    batch_size = s_obs.shape[0]
 
-        qf1_a_values = qf1.forward(s_obs, s_actions).view(-1)
-        qf2_a_values = qf2.forward(s_obs, s_actions).view(-1)
-        qf1_loss = loss_fn(qf1_a_values, next_q_value)
-        qf2_loss = loss_fn(qf2_a_values, next_q_value)
-        qf_loss = (qf1_loss + qf2_loss) / 2 # used for stats only
+    # Standard Bellman update component
+    with torch.no_grad():
+        next_state_actions, next_state_log_pi, _ = pg.get_action(s_next_obses)
+        qf1_next_target = qf1_target.forward(s_next_obses, next_state_actions)
+        qf2_next_target = qf2_target.forward(s_next_obses, next_state_actions)
+        min_qf_next_target = torch.min(qf1_next_target, qf2_next_target) - alpha * next_state_log_pi
+        next_q_value = s_rewards + (1 - s_dones) * args.gamma * min_qf_next_target.view(-1)
 
-        # CQL
-        s_obs_rep = s_obs.repeat_interleave(args.num_actions, 0)
-        s_next_obs_rep = s_next_obses.repeat_interleave(args.num_actions, 0)
-        random_actions_tensor = torch.zeros([args.batch_size * args.num_actions, output_shape]).uniform_(0., 1.).to(device)
-        curr_actions, curr_log_pis, _ = pg.get_action(s_obs_rep)
-        next_actions, next_log_pis, _ = pg.get_action(s_next_obs_rep)
+    qf1_a_values = qf1.forward(s_obs, s_actions).view(-1)
+    qf2_a_values = qf2.forward(s_obs, s_actions).view(-1)
+    qf1_loss = loss_fn(qf1_a_values, next_q_value)
+    qf2_loss = loss_fn(qf2_a_values, next_q_value)
+    qf_loss = (qf1_loss + qf2_loss) / 2 # used for stats only
 
-        q1_rand, q2_rand = qf1(s_obs_rep, random_actions_tensor).view(args.batch_size, args.num_actions), qf2(s_obs_rep, random_actions_tensor).view(args.batch_size, args.num_actions)
-        q1_curr_actions, q2_curr_actions = qf1(s_obs_rep, curr_actions).view(args.batch_size, args.num_actions), qf2(s_obs_rep, curr_actions).view(args.batch_size, args.num_actions)
-        q1_next_actions, q2_next_actions = qf1(s_obs_rep, next_actions).view(args.batch_size, args.num_actions), qf2(s_obs_rep, next_actions).view(args.batch_size, args.num_actions)
+    # CQL
+    s_obs_rep = s_obs.repeat_interleave(args.num_actions, 0)
+    s_next_obs_rep = s_next_obses.repeat_interleave(args.num_actions, 0)
+    random_actions_tensor = torch.zeros([batch_size * args.num_actions, output_shape]).uniform_(0., 1.).to(device)
+    curr_actions, curr_log_pis, _ = pg.get_action(s_obs_rep)
+    next_actions, next_log_pis, _ = pg.get_action(s_next_obs_rep)
 
-        if args.min_q_version == 2:
-            all_q1 = torch.cat([q1_rand, q1_curr_actions, q1_next_actions, qf1_a_values.unsqueeze(-1)], 1)
-            all_q2 = torch.cat([q2_rand, q2_curr_actions, q2_next_actions, qf2_a_values.unsqueeze(-1)], 1)
-        elif args.min_q_version == 3:
-            random_density = np.log(0.5 ** random_actions_tensor.shape[-1])
-            all_q1 = torch.cat([q1_rand - random_density, q1_curr_actions - curr_log_pis.view(args.batch_size, args.num_actions).detach(), q1_next_actions - next_log_pis.view(args.batch_size, args.num_actions).detach()], 1)
-            all_q2 = torch.cat([q2_rand - random_density, q2_curr_actions - curr_log_pis.view(args.batch_size, args.num_actions).detach(), q2_next_actions - next_log_pis.view(args.batch_size, args.num_actions).detach()], 1)
-        else:
-            raise NotImplementedError
+    q1_rand, q2_rand = qf1(s_obs_rep, random_actions_tensor).view(batch_size, args.num_actions), qf2(s_obs_rep, random_actions_tensor).view(batch_size, args.num_actions)
+    q1_curr_actions, q2_curr_actions = qf1(s_obs_rep, curr_actions).view(batch_size, args.num_actions), qf2(s_obs_rep, curr_actions).view(batch_size, args.num_actions)
+    q1_next_actions, q2_next_actions = qf1(s_obs_rep, next_actions).view(batch_size, args.num_actions), qf2(s_obs_rep, next_actions).view(batch_size, args.num_actions)
 
-        min_qf1_loss, min_qf2_loss = torch.logsumexp(all_q1, 1), torch.logsumexp(all_q2, 1)
+    if args.min_q_version == 2:
+        all_q1 = torch.cat([q1_rand, q1_curr_actions, q1_next_actions, qf1_a_values.unsqueeze(-1)], 1)
+        all_q2 = torch.cat([q2_rand, q2_curr_actions, q2_next_actions, qf2_a_values.unsqueeze(-1)], 1)
+    elif args.min_q_version == 3:
+        random_density = np.log(0.5 ** random_actions_tensor.shape[-1])
+        all_q1 = torch.cat([q1_rand - random_density, q1_curr_actions - curr_log_pis.view(batch_size, args.num_actions).detach(), q1_next_actions - next_log_pis.view(batch_size, args.num_actions).detach()], 1)
+        all_q2 = torch.cat([q2_rand - random_density, q2_curr_actions - curr_log_pis.view(batch_size, args.num_actions).detach(), q2_next_actions - next_log_pis.view(batch_size, args.num_actions).detach()], 1)
+    else:
+        raise NotImplementedError
 
-        min_qf1_loss = (min_qf1_loss - qf1_a_values).mean() * args.min_q_weight
-        min_qf2_loss = (min_qf2_loss - qf2_a_values).mean() * args.min_q_weight
+    min_qf1_loss, min_qf2_loss = torch.logsumexp(all_q1, 1), torch.logsumexp(all_q2, 1)
 
-        if args.with_lagrange:
-            alpha_prime = torch.clamp(log_alpha_prime.exp(), min=0.0, max=10000.0)
-            min_qf1_loss = alpha_prime * (min_qf1_loss - args.lagrange_thresh)
-            min_qf2_loss = alpha_prime * (min_qf2_loss - args.lagrange_thresh)
+    min_qf1_loss = (min_qf1_loss - qf1_a_values).mean() * args.min_q_weight
+    min_qf2_loss = (min_qf2_loss - qf2_a_values).mean() * args.min_q_weight
 
-            alpha_prime_optimizer.zero_grad()
-            alpha_prime_loss = -.5 * (min_qf1_loss + min_qf2_loss)
-            alpha_prime_loss.backward(retain_graph=True)
-            alpha_prime_optimizer.step()
-        
-        cql_qf1_loss = qf1_loss + min_qf1_loss
-        cql_qf2_loss = qf2_loss + min_qf2_loss
+    if args.with_lagrange:
+        alpha_prime = torch.clamp(log_alpha_prime.exp(), min=0.0, max=10000.0)
+        min_qf1_loss = alpha_prime * (min_qf1_loss - args.lagrange_thresh)
+        min_qf2_loss = alpha_prime * (min_qf2_loss - args.lagrange_thresh)
 
-        cql_qf_loss = (cql_qf1_loss + cql_qf2_loss) / 2.
-
-        values_optimizer.zero_grad()
-        cql_qf_loss.backward()
-        values_optimizer.step()
-
-        ## Policy network update
-        if global_step % args.policy_frequency == 0: # TD 3 Delayed update support
-            for _ in range(args.policy_frequency): # compensate for the delay by doing 'actor_update_interval' instead of 1
-                pi, log_pi, _ = pg.get_action(s_obs)
-                qf1_pi = qf1.forward(s_obs, pi)
-                qf2_pi = qf2.forward(s_obs, pi)
-                min_qf_pi = torch.min(qf1_pi, qf2_pi).view(-1)
-                policy_loss = ((alpha * log_pi) - min_qf_pi).mean()
-
-                policy_optimizer.zero_grad()
-                policy_loss.backward()
-                policy_optimizer.step()
-            
-            if args.autotune:
-                with torch.no_grad():
-                    _, log_pi, _ = pg.get_action(s_obs)
-                alpha_loss = ( -log_alpha * (log_pi + target_entropy)).mean()
-
-                a_optimizer.zero_grad()
-                alpha_loss.backward()
-                a_optimizer.step()
-                alpha = log_alpha.exp().item()
-            
-        # update the target network
-        if global_step % args.target_network_frequency == 0:
-            for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
-                target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
-            for param, target_param in zip(qf2.parameters(), qf2_target.parameters()):
-                target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
-        
-        # Stop iteration over the dataset
-        if k >= args.epoch_length-1:
-            break
+        alpha_prime_optimizer.zero_grad()
+        alpha_prime_loss = -.5 * (min_qf1_loss + min_qf2_loss)
+        alpha_prime_loss.backward(retain_graph=True)
+        alpha_prime_optimizer.step()
     
-    eval_stats = test_agent(env, pg)
-    print("[E%4d|I%8d] -- PLoss: %.3f -- QLoss: %.3f -- CQLQloss: %.3f --  Train Mean Ret: %.3f" %
-        (epoch, global_step, policy_loss.item(), qf_loss.item(), cql_qf_loss.item(),
-        eval_stats["test_mean_return"]))
-    if args.with_lagrange:
-        print("\t APrimeLoss: %.3f" % alpha_prime_loss.item())
+    cql_qf1_loss = qf1_loss + min_qf1_loss
+    cql_qf2_loss = qf2_loss + min_qf2_loss
 
-    writer.add_scalar("losses/soft_q_value_1_loss", qf1_loss.item(), global_step)
-    writer.add_scalar("losses/soft_q_value_2_loss", qf2_loss.item(), global_step)
-    writer.add_scalar("losses/qf_loss", qf_loss.item(), global_step)
-    writer.add_scalar("losses/cql_qf_loss", cql_qf_loss.item(), global_step)
-    writer.add_scalar("losses/policy_loss", policy_loss.item(), global_step)
-    writer.add_scalar("losses/alpha", alpha, global_step)
-    writer.add_scalar("charts/episode_reward", eval_stats["test_mean_return"], global_step)
-    writer.add_scalar("global_step", global_step, global_step)
-    if args.autotune:
-        writer.add_scalar("losses/alpha_loss", alpha_loss.item(), global_step)
-    if args.with_lagrange:
-        writer.add_scalar("losses/alpha_prime_loss", alpha_prime_loss.item(), global_step)
-        writer.add_scalar("losses/alpha_prime", log_alpha_prime.exp().item(), global_step)
+    cql_qf_loss = (cql_qf1_loss + cql_qf2_loss) / 2.
+
+    values_optimizer.zero_grad()
+    cql_qf_loss.backward()
+    values_optimizer.step()
+
+    ## Policy network update
+    if global_step % args.policy_frequency == 0: # TD 3 Delayed update support
+        for _ in range(args.policy_frequency): # compensate for the delay by doing 'actor_update_interval' instead of 1
+            pi, log_pi, _ = pg.get_action(s_obs)
+            qf1_pi = qf1.forward(s_obs, pi)
+            qf2_pi = qf2.forward(s_obs, pi)
+            min_qf_pi = torch.min(qf1_pi, qf2_pi).view(-1)
+            policy_loss = ((alpha * log_pi) - min_qf_pi).mean()
+
+            policy_optimizer.zero_grad()
+            policy_loss.backward()
+            policy_optimizer.step()
+        
+        if args.autotune:
+            with torch.no_grad():
+                _, log_pi, _ = pg.get_action(s_obs)
+            alpha_loss = ( -log_alpha * (log_pi + target_entropy)).mean()
+
+            a_optimizer.zero_grad()
+            alpha_loss.backward()
+            a_optimizer.step()
+            alpha = log_alpha.exp().item()
+        
+    # update the target network
+    if global_step % args.target_network_frequency == 0:
+        for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
+            target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+        for param, target_param in zip(qf2.parameters(), qf2_target.parameters()):
+            target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+    
+    # evaluating the agent
+    if global_step % args.eval_frequency == 0:
+        eval_returns = []
+        for _ in range(args.n_eval_episodes):
+            ret, done, t = 0., False, 0
+            obs = np.array(env.reset())
+            while not done:
+                with torch.no_grad():
+                    action, _, _ = pg.get_action(torch.Tensor(obs).unsqueeze(0).to(device))
+                    action = action.tolist()[0]
+
+                obs, rew, done, _ = env.step( action)
+                obs = np.array( obs)
+                ret += rew
+                t += 1
+            eval_returns.append(ret)
+        avg_eval_return = np.mean(eval_returns)
+
+        print(f"global_step={global_step}, episode_reward={avg_eval_return}")
+        if args.with_lagrange:
+            print("\t APrimeLoss: %.3f" % alpha_prime_loss.item())
+
+        writer.add_scalar("losses/soft_q_value_1_loss", qf1_loss.item(), global_step)
+        writer.add_scalar("losses/soft_q_value_2_loss", qf2_loss.item(), global_step)
+        writer.add_scalar("losses/qf_loss", qf_loss.item(), global_step)
+        writer.add_scalar("losses/cql_qf_loss", cql_qf_loss.item(), global_step)
+        writer.add_scalar("losses/policy_loss", policy_loss.item(), global_step)
+        writer.add_scalar("losses/alpha", alpha, global_step)
+        writer.add_scalar("charts/episode_reward", avg_eval_return, global_step)
+        writer.add_scalar("global_step", global_step, global_step)
+        if args.autotune:
+            writer.add_scalar("losses/alpha_loss", alpha_loss.item(), global_step)
+        if args.with_lagrange:
+            writer.add_scalar("losses/alpha_prime_loss", alpha_prime_loss.item(), global_step)
+            writer.add_scalar("losses/alpha_prime", log_alpha_prime.exp().item(), global_step)
 
 writer.close()
 env.close()
