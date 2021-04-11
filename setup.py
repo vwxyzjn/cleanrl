3e481d55edbce99091ea76cbc8dd502ecdec47b2
Costa Huang <costa.huang@outlook.com>
2021-04-11 18:27:15 -0400

    include versioneer.py

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup, find_packages
 
-setup(name='cleanrl',
-      version='0.4.0',
-      install_requires=['gym', 'torch', 'tensorboard', 'wandb', 'stable_baselines3'],
-      packages=find_packages()
+extras = {
+  'gym_all': ['gym[all]', 'opencv-python'],
+  'pybullet': ['pybullet'],
+  'pysc2': ['pysc2'],
+  'procgen': ['procgen']
+}
+
+extras['all'] = [item for group in extras.values() for item in group]
+
+setup(
+    name='cleanrl',
+    install_requires=[
+        'gym',
+        'torch',
+        'tensorboard',
+        'wandb',
+        'stable_baselines3',
+        'seaborn',],
+    extras_require=extras,
+    packages=find_packages(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
