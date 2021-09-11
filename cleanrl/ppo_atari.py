6c1811f5d304fba136f86c213a4eb4e0405447c5
Costa Huang <costa.huang@outlook.com>
2021-09-11 18:54:46 -0400

    quick fix

--- cleanrl/ppo_atari.py
+++ cleanrl/ppo_atari.py
@@ -36,7 +36,7 @@ def parse_args():
     parser.add_argument('--torch-deterministic', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
         help='if toggled, `torch.backends.cudnn.deterministic=False`')
     parser.add_argument('--cuda', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
-        help='if toggled, cuda will not be enabled by default')
+        help='if toggled, cuda will be enabled by default')
     parser.add_argument('--track', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
         help='if toggled, this experiment will be tracked with Weights and Biases')
     parser.add_argument('--wandb-project-name', type=str, default="cleanRL",
