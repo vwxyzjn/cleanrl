94a1905914460a29dec208ea67cf561f36369c04
Costa Huang <costa.huang@outlook.com>
2021-09-11 18:57:12 -0400

    deprecate stuff

--- cleanrl/experiments/save_conda.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import conda.cli
-is_conda = os.path.exists(os.path.join(sys.prefix, 'conda-meta'))
-
-if is_conda:
-    conda_env = os.environ['CONDA_DEFAULT_ENV']
-    conda.cli.main("conda", "env", "export")
--- cleanrl/experiments/test.md
+++
@@ -1,1 +0,0 @@
-![ezgif-7-bc2b7f0c5c8b](https://user-images.githubusercontent.com/5555347/121985930-acd48f80-cd63-11eb-9f8a-0eec0bacd283.gif)
