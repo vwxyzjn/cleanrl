af57044891440299909b13d91d8e430285329293
Costa Huang <costa.huang@outlook.com>
2021-11-02 19:59:04 -0400

    update test cases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ import subprocess
 
 def test_submit_exp():
     subprocess.run(
-        "poetry run python -m cleanrl_utils.submit_exp --docker-tag vwxyzjn/cleanrl:latest",
+        "poetry run python -m cleanrl_utils.submit_exp --docker-tag vwxyzjn/cleanrl:latest --wandb-key xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
         shell=True,
         check=True,
     )
