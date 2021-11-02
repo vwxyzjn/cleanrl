f8377f0f74b34359f8eab590adabbe2d8e6154f1
Costa Huang <costa.huang@outlook.com>
2021-11-02 19:15:32 -0400

    update files

--- docker/build.sh
+++
@@ -1,5 +0,0 @@
-VERSION=1.4-cuda10.1-cudnn7-runtime-cleanrl-0.3.1
-
-docker build -t vwxyzjn/cleanrl:$VERSION  -t vwxyzjn/cleanrl:latest -f Dockerfile .
-docker push vwxyzjn/cleanrl:latest
-docker push vwxyzjn/cleanrl:$VERSION
