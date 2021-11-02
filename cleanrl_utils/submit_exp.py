b1c872996986c2afc67add2dd06ca90137f254ee
Costa Huang <costa.huang@outlook.com>
2021-11-02 19:52:52 -0400

    fix format

--- cleanrl_utils/submit_exp.py
+++ cleanrl_utils/submit_exp.py
@@ -1,10 +1,11 @@
-import boto3
-import time
-import requests
 import argparse
-import subprocess
 import multiprocessing
+import subprocess
+import time
 from distutils.util import strtobool
+
+import boto3
+import requests
 
 # fmt: off
 parser = argparse.ArgumentParser(description='CleanRL Experiment Submission')
@@ -61,15 +62,20 @@ assert len(args.wandb_key) > 0, "you have not logged into W&B; try do `wandb log
 
 # extract runs from bash scripts
 final_run_cmds = []
-for seed in range(1,1+args.num_seed):
+for seed in range(1, 1 + args.num_seed):
     final_run_cmds += [args.command + " --seed " + str(seed)]
 
 final_str = ""
 cores = multiprocessing.cpu_count()
 current_core = 0
 for final_run_cmd in final_run_cmds:
-    run_command = (f'docker run -d --cpuset-cpus="{current_core}" -e WANDB_API_KEY={args.wandb_key} {args.docker_tag} ' + 
-        '/bin/bash -c "' + final_run_cmd + '"' + "\n")
+    run_command = (
+        f'docker run -d --cpuset-cpus="{current_core}" -e WANDB_API_KEY={args.wandb_key} {args.docker_tag} '
+        + '/bin/bash -c "'
+        + final_run_cmd
+        + '"'
+        + "\n"
+    )
     print(run_command)
     final_str += run_command
     current_core = (current_core + 1) % cores
@@ -79,60 +85,53 @@ with open(f"{args.exp_script}.docker.sh", "w+") as f:
 
 # submit jobs
 if args.provider == "aws":
-    client = boto3.client('batch')
+    client = boto3.client("batch")
     for final_run_cmd in final_run_cmds:
-        job_name = args.command.replace(".py", "").replace("/", "_").replace(" ", "").replace("-", "_") + str(int(time.time()))
+        job_name = args.command.replace(".py", "").replace("/", "_").replace(" ", "").replace("-", "_") + str(
+            int(time.time())
+        )
         resources_requirements = []
         if args.num_gpu:
             resources_requirements = [
-                {
-                    'value': str(args.num_gpu),
-                    'type': 'GPU'
-                },
+                {"value": str(args.num_gpu), "type": "GPU"},
             ]
         try:
             job_def_name = args.docker_tag.replace(":", "_").replace("/", "_")
             job_def = client.register_job_definition(
                 jobDefinitionName=job_def_name,
-                type='container',
+                type="container",
                 containerProperties={
-                    'image': args.docker_tag,
-                    'vcpus': args.num_vcpu,
-                    'memory': args.num_memory,
-                    'command': [
-                        '/bin/bash',
+                    "image": args.docker_tag,
+                    "vcpus": args.num_vcpu,
+                    "memory": args.num_memory,
+                    "command": [
+                        "/bin/bash",
                     ],
-                }
+                },
             )
             response = client.submit_job(
                 jobName=job_name,
                 jobQueue=args.job_queue,
                 jobDefinition=job_def_name,
                 containerOverrides={
-                    'vcpus': args.num_vcpu,
-                    'memory': args.num_memory,
-                    'command': ["/bin/bash", "-c", final_run_cmd],
-                    'environment': [
-                        {'name': 'WANDB_API_KEY', 'value': args.wandb_key},
+                    "vcpus": args.num_vcpu,
+                    "memory": args.num_memory,
+                    "command": ["/bin/bash", "-c", final_run_cmd],
+                    "environment": [
+                        {"name": "WANDB_API_KEY", "value": args.wandb_key},
                     ],
-                    'resourceRequirements': resources_requirements,
+                    "resourceRequirements": resources_requirements,
                 },
-                retryStrategy={
-                    'attempts': 1
-                },
-                timeout={
-                    'attemptDurationSeconds': int(args.num_hours*60*60)
-                }
+                retryStrategy={"attempts": 1},
+                timeout={"attemptDurationSeconds": int(args.num_hours * 60 * 60)},
             )
-            if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
                 print(response)
                 raise Exception("jobs submit failure")
         except Exception as e:
             print(e)
         finally:
-            response = client.deregister_job_definition(
-                jobDefinition=job_def_name
-            )
-            if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            response = client.deregister_job_definition(jobDefinition=job_def_name)
+            if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
                 print(response)
                 raise Exception("jobs submit failure")
