c86010e741618bdde719c57c1f7856858e9334e5
Costa Huang <costa.huang@outlook.com>
2021-11-02 19:48:01 -0400

    add a test case

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+def test_submit_exp():
+    subprocess.run(
+        "poetry run python -m cleanrl_utils.submit_exp --docker-tag vwxyzjn/cleanrl:latest",
+        shell=True,
+        check=True,
+    )
