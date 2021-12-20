63982c291fa12d82301f54fa3dbe32640c32f1fc
Niju Nicholas <niju.nicholas@gmail.com>
2021-12-20 21:57:18 +0000

    code cleanup to match vdqn

--- cleanrl/atari/v_c51_atari.py
+++ cleanrl/atari/v_c51_atari.py
@@ -1,13 +1,18 @@
-# https://github.com/facebookresearch/torchbeast/blob/master/torchbeast/core/environment.py
+#C51 Paper: https://arxiv.org/pdf/1707.06887.pdf
+#Paper Implementation: https://github.com/vwxyzjn/cleanrl/blob/master/cleanrl/c51_atari.py
+#Vectorized : https://github.com/vwxyzjn/cleanrl/blob/experimental-vdqn/cleanrl/atari/vdqn_atari.py
 
-#Vectoried implementation of c51 based on vdqn_atari.py
+import argparse
+import os
+import random
+import time
+from distutils.util import strtobool
+import gym
 
 import numpy as np
-from collections import deque
-import gym
-from gym import spaces
-import cv2
-cv2.ocl.setUseOpenCL(False)
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from stable_baselines3.common.atari_wrappers import (
     ClipRewardEnv,
     EpisodicLifeEnv,
@@ -15,26 +20,7 @@ from stable_baselines3.common.atari_wrappers import (
     MaxAndSkipEnv,
     NoopResetEnv,
 )
-
-# Reference: https://arxiv.org/pdf/1707.06887.pdf
-# https://github.com/ShangtongZhang/DeepRL/blob/master/deep_rl/agent/CategoricalDQN_agent.py
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
-
-import argparse
-from distutils.util import strtobool
-import collections
-import numpy as np
-import gym
-from gym.wrappers import TimeLimit, Monitor
-from gym.spaces import Discrete, Box, MultiBinary, MultiDiscrete, Space
-import time
-import random
-import os
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Vectorized-C51 agent')
@@ -54,13 +40,13 @@ def parse_args():
     parser.add_argument('--cuda', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
                         help='if toggled, cuda will not be enabled by default')
     parser.add_argument('--track', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
-                        help='run the script in production mode and use wandb to log outputs')
-    parser.add_argument('--capture-video', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
-                        help='weather to capture videos of the agent performances (check out `videos` folder)')
+                        help='if toggled, this experiment will be tracked with Weights and Biases')
     parser.add_argument('--wandb-project-name', type=str, default="cleanRL",
                         help="the wandb's project name")
     parser.add_argument('--wandb-entity', type=str, default=None,
                         help="the entity (team) of wandb's project")
+    parser.add_argument('--capture-video', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
+                        help='weather to capture videos of the agent performances (check out `videos` folder)')
     
     # Algorithm specific arguments
     parser.add_argument('--num-envs', type=int, default=8,
@@ -71,35 +57,28 @@ def parse_args():
         help='the number of mini-batches')
     parser.add_argument('--update-epochs', type=int, default=16,
         help="the K epochs to update the policy")
+    parser.add_argument('--gamma', type=float, default=0.99,
+                        help='the discount factor gamma')
     parser.add_argument('--n-atoms', type=int, default=51,
                         help="the number of atoms")
     parser.add_argument('--v-min', type=float, default=-10,
                         help="the number of atoms")
     parser.add_argument('--v-max', type=float, default=10,
                         help="the number of atoms")
-    parser.add_argument('--gamma', type=float, default=0.99,
-                        help='the discount factor gamma')
     parser.add_argument('--target-network-frequency', type=int, default=1000,
                         help="the timesteps it takes to update the target network")
     parser.add_argument('--max-grad-norm', type=float, default=0.5,
                         help='the maximum norm for the gradient clipping')
-    parser.add_argument('--batch-size', type=int, default=32,
-                        help="the batch size of sample from the reply memory")
     parser.add_argument('--start-e', type=float, default=1.,
                         help="the starting epsilon for exploration")
     parser.add_argument('--end-e', type=float, default=0.01,
                         help="the ending epsilon for exploration")
     parser.add_argument('--exploration-fraction', type=float, default=0.10,
                         help="the fraction of `total-timesteps` it takes from start-e to go end-e")
-    parser.add_argument('--learning-starts', type=int, default=80000,
-                        help="timestep to start learning")
-    parser.add_argument('--train-frequency', type=int, default=4,
-                        help="the frequency of training")
+
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
-    if not args.seed:
-        args.seed = int(time.time())
     return args
 
 # TRY NOT TO MODIFY: setup the environment
@@ -148,7 +127,6 @@ class QNetwork(nn.Module):
         )
 
     def forward(self, x, device):
-        #x = torch.Tensor(x).to(device)
         return self.network(x/255.0)
 
     def get_action(self, envs, device, x,action=None):
@@ -176,28 +154,34 @@ if __name__ == "__main__":
             '\n'.join([f"|{key}|{value}|" for key, value in vars(args).items()])))
     if args.track:
         import wandb
-        wandb.init(project=args.wandb_project_name, entity=args.wandb_entity, sync_tensorboard=True, config=vars(args), name=run_name, monitor_gym=True, save_code=True)
-        writer = SummaryWriter(f"/tmp/{run_name}")
+        wandb.init(project=args.wandb_project_name, 
+                   entity=args.wandb_entity,
+                   sync_tensorboard=True,
+                   config=vars(args),
+                   name=run_name,
+                   monitor_gym=True,
+                   save_code=True)
 
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+    
+    device = torch.device('cuda' if torch.cuda.is_available() and args.cuda else 'cpu')
+
+    # env setup
     envs = gym.vector.SyncVectorEnv(
         [make_env(args.gym_id, args.seed + i, i, args.capture_video, run_name) for i in range(args.num_envs)]
     )
     assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
 
-    # TRY NOT TO MODIFY: seeding
-    device = torch.device('cuda' if torch.cuda.is_available() and args.cuda else 'cpu')
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.backends.cudnn.deterministic = args.torch_deterministic
-
+    # ALGO LOGIC: initialize agent here:
     q_network = QNetwork(envs, device, n_atoms=args.n_atoms, v_min=args.v_min, v_max=args.v_max).to(device)
     target_network = QNetwork(envs, device, n_atoms=args.n_atoms, v_min=args.v_min, v_max=args.v_max).to(device)
     target_network.load_state_dict(q_network.state_dict())
     optimizer = optim.Adam(q_network.parameters(), lr=args.learning_rate, eps=0.01/args.batch_size)
     loss_fn = nn.MSELoss()
-    print(device.__repr__())
-    print(q_network)
 
     # ALGO Logic: Storage setup
     obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
@@ -206,7 +190,6 @@ if __name__ == "__main__":
     dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
 
     # TRY NOT TO MODIFY: start the game
-
     global_step = 0
     start_time = time.time()
     next_obs = torch.Tensor(envs.reset()).to(device)
@@ -222,25 +205,24 @@ if __name__ == "__main__":
             global_step += 1 * args.num_envs
             obs[step] = next_obs
             dones[step] = next_done
+            # ALGO LOGIC: action logic
             with torch.no_grad():
                 action, _ = q_network.get_action(envs, device, next_obs)
             actions[step] = action
             
             # TRY NOT TO MODIFY: execute the game and log data.
-            #print(action.shape)
             next_obs, reward, done, info = envs.step(action.cpu().numpy())
             rewards[step] = torch.tensor(reward).to(device).view(-1)
             next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(done).to(device)
 
-            # TRY NOT TO MODIFY: record rewards for plotting purposes
             for item in info:
-                if 'episode' in item.keys():
-                    print(f"global_step={global_step}, episode_reward={item['episode']['r']}")
-                    writer.add_scalar("charts/episodic_return", item['episode']['r'], global_step)
+                if "episode" in item.keys():
+                    print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
+                    writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
                     writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
                     break
 
-        # ALGO LOGIC: training.
+        # TRAINING
         b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
         b_actions = actions.reshape((-1, 1)).long()
         b_rewards = rewards.reshape((-1,))
@@ -296,9 +278,6 @@ if __name__ == "__main__":
                 # update the target network
                 if num_gradient_updates % args.target_network_frequency == 0:
                     target_network.load_state_dict(q_network.state_dict())
-
-        
-
 
     envs.close()
     writer.close()
