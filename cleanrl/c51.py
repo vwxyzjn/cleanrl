0747aaa001aef6e940546f1913cc4897ef188112
Costa Huang <costa.huang@outlook.com>
2022-01-31 14:36:25 -0500

    Quick update

--- cleanrl/c51.py
+++ cleanrl/c51.py
@@ -17,7 +17,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
         help="the name of this experiment")
-    parser.add_argument("--gym-id", type=str, default="CartPole-v0",
+    parser.add_argument("--gym-id", type=str, default="CartPole-v1",
         help="the id of the gym environment")
     parser.add_argument("--learning-rate", type=float, default=2.5e-4,
         help="the learning rate of the optimizer")
