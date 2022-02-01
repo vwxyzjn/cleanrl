bc9bdf16a92982575f1f4c18af4ebdced9c8e4c9
dosssman <dosssman@hotmail.fr>
2022-02-01 18:27:36 +0900

    TD3: removed DDPG's update code from the training loop

--- cleanrl/td3_continuous_action.py
+++ cleanrl/td3_continuous_action.py
@@ -198,42 +198,6 @@ if __name__ == "__main__":
         if global_step > args.learning_starts:
             data = rb.sample(args.batch_size)
             with torch.no_grad():
-                next_state_actions = (
-                    target_actor.forward(data.next_observations)
-                ).clamp(envs.single_action_space.low[0], envs.single_action_space.high[0])
-                qf1_next_target = qf1_target.forward(data.next_observations, next_state_actions)
-                next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (qf1_next_target).view(-1)
-
-            qf1_a_values = qf1.forward(data.observations, data.actions).view(-1)
-            qf1_loss = loss_fn(qf1_a_values, next_q_value)
-
-            # optimize the midel
-            q_optimizer.zero_grad()
-            qf1_loss.backward()
-            nn.utils.clip_grad_norm_(list(qf1.parameters()), args.max_grad_norm)
-            q_optimizer.step()
-
-            if global_step % args.policy_frequency == 0:
-                actor_loss = -qf1.forward(data.observations, actor.forward(data.observations)).mean()
-                actor_optimizer.zero_grad()
-                actor_loss.backward()
-                nn.utils.clip_grad_norm_(list(actor.parameters()), args.max_grad_norm)
-                actor_optimizer.step()
-
-                # update the target network
-                for param, target_param in zip(actor.parameters(), target_actor.parameters()):
-                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
-                for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
-                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
-
-            if global_step % 100 == 0:
-                writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
-                writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
-
-
-
-
-            with torch.no_grad():
                 clipped_noise = (
                     torch.randn_like(torch.Tensor(actions[0])) * args.policy_noise
                 ).clamp(-args.noise_clip, args.noise_clip)
@@ -251,7 +215,7 @@ if __name__ == "__main__":
             qf1_loss = loss_fn(qf1_a_values, next_q_value)
             qf2_loss = loss_fn(qf2_a_values, next_q_value)
 
-            # optimize the midel
+            # optimize the model
             q_optimizer.zero_grad()
             qf1_loss.backward()
             qf2_loss.backward()
