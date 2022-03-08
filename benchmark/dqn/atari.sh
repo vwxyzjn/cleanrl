b1801b3f0758a8b0f8f0226ff77531c7e10285f4
Costa Huang <costa.huang@outlook.com>
2022-03-08 17:55:11 -0500

    Add reproduce scripts

---
+++ benchmark/dqn/atari.sh
@@ -0,0 +1,14 @@
+# PongNoFrameskip-v4
+poetry run python cleanrl/dqn_atari.py --gym-id PongNoFrameskip-v4 --track --seed 1 --wandb-project-name cleanrl--wandb-entity openrlbenchmark
+poetry run python cleanrl/dqn_atari.py --gym-id PongNoFrameskip-v4 --track --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+poetry run python cleanrl/dqn_atari.py --gym-id PongNoFrameskip-v4 --track --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+
+# BeamRiderNoFrameskip-v4
+poetry run python cleanrl/dqn_atari.py --gym-id BeamRiderNoFrameskip-v4 --track --seed 1 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+poetry run python cleanrl/dqn_atari.py --gym-id BeamRiderNoFrameskip-v4 --track --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+poetry run python cleanrl/dqn_atari.py --gym-id BeamRiderNoFrameskip-v4 --track --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+
+# BreakoutNoFrameskip-v4
+poetry run python cleanrl/dqn_atari.py --gym-id BreakoutNoFrameskip-v4 --track --seed 1 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+poetry run python cleanrl/dqn_atari.py --gym-id BreakoutNoFrameskip-v4 --track --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+poetry run python cleanrl/dqn_atari.py --gym-id BreakoutNoFrameskip-v4 --track --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
