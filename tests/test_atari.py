bd7ab643d129152bf4355a45b8885a76b7257565
dosssman <dosssman@hotmail.fr>
2022-03-09 18:29:16 +0900

    Addtional tweaks regarding SPS and Q values, added tests for Atari related scripts while at it

--- tests/test_atari.py
+++ tests/test_atari.py
@@ -16,10 +16,23 @@ def test_dqn():
         check=True,
     )
 
+def test_apex_dqn_atari():
+    subprocess.run(
+        "python cleanrl/c51_atari.py --learning-starts 10 --total-timesteps 16 --buffer-size 10 --batch-size 4",
+        shell=True,
+        check=True,
+    )
 
 def test_c51():
     subprocess.run(
         "python cleanrl/c51_atari.py --learning-starts 10 --total-timesteps 16 --buffer-size 10 --batch-size 4",
+        shell=True,
+        check=True,
+    )
+
+def test_rnd_ppo():
+    subprocess.run(
+        "python cleanrl/rnd_ppo.py --num-envs 1 --num-steps 64 --total-timesteps 256",
         shell=True,
         check=True,
     )
