04e24061a9af12329a80807f561f43d8660d5f17
Costa Huang <costa.huang@outlook.com>
2022-04-13 18:20:33 -0400

    Let `ppo_continuous_action.py`only run 1M steps

--- cleanrl/ppo_continuous_action.py
+++ cleanrl/ppo_continuous_action.py
@@ -37,7 +37,7 @@ def parse_args():
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="HalfCheetahBulletEnv-v0",
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=2000000,
+    parser.add_argument("--total-timesteps", type=int, default=1000000,
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=3e-4,
         help="the learning rate of the optimizer")
