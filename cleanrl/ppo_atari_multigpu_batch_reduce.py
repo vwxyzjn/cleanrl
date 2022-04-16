Fix incorrect `num_updates` calculation

cleanrl/ppo_atari_multigpu_batch_reduce.py
@@ -214,7 +214,7 @@ def train(rank: int, size: int):
     start_time = time.time()
     next_obs = torch.Tensor(envs.reset()).to(device)
     next_done = torch.zeros(args.num_envs).to(device)
-    num_updates = args.total_timesteps // args.batch_size
+    num_updates = args.total_timesteps // (args.batch_size * size)
 
     for update in range(1, num_updates + 1):
         # Annealing the rate if instructed to do so.
@@ -376,7 +376,6 @@ def train(rank: int, size: int):
 
 
 if __name__ == "__main__":
-    # train(1)
     size = 2
     processes = []
     mp.set_start_method("spawn")

cleanrl/ppo_atari_multigpu_dpp.py
@@ -216,7 +216,7 @@ def train(rank: int, size: int):
     start_time = time.time()
     next_obs = torch.Tensor(envs.reset()).to(device)
     next_done = torch.zeros(args.num_envs).to(device)
-    num_updates = args.total_timesteps // args.batch_size
+    num_updates = args.total_timesteps // (args.batch_size * size)
 
     for update in range(1, num_updates + 1):
         # Annealing the rate if instructed to do so.