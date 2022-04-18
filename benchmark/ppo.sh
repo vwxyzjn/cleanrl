594ab1a3a1fa9f646f88eb7a11646c7c4b2c272b
Costa Huang <costa.huang@outlook.com>
2022-04-17 22:38:04 -0400

    update benchmark scripts

--- benchmark/ppo.sh
+++ benchmark/ppo.sh
@@ -4,28 +4,28 @@
 poetry install
 OMP_NUM_THREADS=1 python -m cleanrl_utils.benchmark \
     --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 \
-    --command "poetry run python cleanrl/ppo.py --cuda False --track" \
+    --command "poetry run python cleanrl/ppo.py --cuda False --track --capture-video" \
+    --num-seeds 3 \
+    --workers 9
+
+poetry install -E atari
+OMP_NUM_THREADS=1 python -m cleanrl_utils.benchmark \
+    --env-ids PongNoFrameskip-v4 BeamRiderNoFrameskip-v4 BreakoutNoFrameskip-v4 \
+    --command "poetry run python cleanrl/ppo_atari.py --track --capture-video" \
     --num-seeds 3 \
     --workers 3
 
 poetry install -E atari
-python -m cleanrl_utils.benchmark \
+OMP_NUM_THREADS=1 python -m cleanrl_utils.benchmark \
     --env-ids PongNoFrameskip-v4 BeamRiderNoFrameskip-v4 BreakoutNoFrameskip-v4 \
-    --command "poetry run python cleanrl/ppo_atari.py --track" \
+    --command "poetry run python cleanrl/ppo_atari_lstm.py --track --capture-video" \
     --num-seeds 3 \
-    --workers 1
+    --workers 3
 
-poetry install -E atari
+poetry install -E envpool
 python -m cleanrl_utils.benchmark \
-    --env-ids PongNoFrameskip-v4 BeamRiderNoFrameskip-v4 BreakoutNoFrameskip-v4 \
-    --command "poetry run python cleanrl/ppo_atari_lstm.py --track" \
-    --num-seeds 3 \
-    --workers 1
-
-poetry install -E atari
-python -m cleanrl_utils.benchmark \
-    --env-ids PongNoFrameskip-v4 BeamRiderNoFrameskip-v4 BreakoutNoFrameskip-v4 \
-    --command "poetry run python cleanrl/ppo_atari_envpool.py --track" \
+    --env-ids Pong-v5 BeamRider-v5 Breakout-v5 \
+    --command "poetry run python cleanrl/ppo_atari_envpool.py --track --capture-video" \
     --num-seeds 3 \
     --workers 1
 
@@ -33,13 +33,13 @@ poetry install -E "mujoco pybullet"
 python -c "import mujoco_py"
 OMP_NUM_THREADS=1 python -m cleanrl_utils.benchmark \
     --env-ids HalfCheetah-v2 Walker2d-v2 Hopper-v2 \
-    --command "poetry run python cleanrl/ppo_continuous_action.py --cuda False --track" \
+    --command "poetry run python cleanrl/ppo_continuous_action.py --cuda False --track --capture-video" \
     --num-seeds 3 \
-    --workers 3
+    --workers 9
 
 poetry install -E procgen
 python -m cleanrl_utils.benchmark \
     --env-ids starpilot bossfight bigfish \
-    --command "poetry run python cleanrl/ppo_procgen.py --track" \
+    --command "poetry run python cleanrl/ppo_procgen.py --track --capture-video" \
     --num-seeds 3 \
     --workers 1
