6d6bed65e4c34330edc5d4b5f09ed232d8ee509b
Costa Huang <costa.huang@outlook.com>
2022-04-17 22:03:57 -0400

    Change `ppo.py`'s default timesteps

--- cleanrl/ppo.py
+++ cleanrl/ppo.py
@@ -39,7 +39,7 @@ def parse_args():
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="CartPole-v1",
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=25000,
+    parser.add_argument("--total-timesteps", type=int, default=500000,
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=2.5e-4,
         help="the learning rate of the optimizer")
