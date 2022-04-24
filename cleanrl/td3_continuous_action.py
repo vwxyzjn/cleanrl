cf17e2e2f3964fdb202cf2c7951ebc841e577073
Costa Huang <costa.huang@outlook.com>
2022-04-24 19:56:35 -0400

    Fix #172

--- cleanrl/td3_continuous_action.py
+++ cleanrl/td3_continuous_action.py
@@ -250,8 +250,10 @@ if __name__ == "__main__":
 
             if global_step % 100 == 0:
                 writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
-                writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
                 writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
+                writer.add_scalar("losses/qf2_loss", qf2_loss.item(), global_step)
+                writer.add_scalar("losses/qf2_values", qf2_a_values.mean().item(), global_step)
+                writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
                 print("SPS:", int(global_step / (time.time() - start_time)))
                 writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
 
