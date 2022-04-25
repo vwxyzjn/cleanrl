de9f48d2df4e3084dcd0d9ffd2eb83691ed64115
Costa Huang <costa.huang@outlook.com>
2022-04-24 21:49:06 -0400

    Fix pre-commit

--- cleanrl_utils/add_header.py
+++ cleanrl_utils/add_header.py
@@ -1,16 +1,17 @@
 import os
 
+
 def add_header(dirname: str):
     """
     Add a header string with documentation link
     to each file in the directory `dirname`.
     """
-    
+
     for filename in os.listdir(dirname):
-        if filename.endswith('.py'):
-            with open(os.path.join(dirname, filename), 'r') as f:
+        if filename.endswith(".py"):
+            with open(os.path.join(dirname, filename)) as f:
                 lines = f.readlines()
-            
+
             # hacky bit
             exp_name = filename.split(".")[0]
             algo_name = exp_name.split("_")[0]
@@ -18,9 +19,10 @@ def add_header(dirname: str):
 
             if not lines[0].startswith(header_string):
                 print(f"adding headers for {filename}")
-                lines.insert(0, header_string + '\n')
-                with open(os.path.join(dirname, filename), 'w') as f:
+                lines.insert(0, header_string + "\n")
+                with open(os.path.join(dirname, filename), "w") as f:
                     f.writelines(lines)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     add_header("cleanrl")
