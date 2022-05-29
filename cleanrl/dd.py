f127aa3d5958917d44184b2cda4583be021d84b6
Costa Huang <costa.huang@outlook.com>
2022-05-29 13:40:57 -0400

    prototype jax with ddpg

---
+++ cleanrl/dd.py
@@ -0,0 +1,23 @@
+from typing import Sequence
+
+import numpy as np
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+
+class MLP(nn.Module):
+  features: Sequence[int]
+
+  @nn.compact
+  def __call__(self, x):
+    for feat in self.features[:-1]:
+      x = nn.relu(nn.Dense(feat)(x))
+    x = nn.Dense(self.features[-1])(x)
+    return x
+
+model = MLP([12, 8, 4])
+batch = jnp.ones((32, 10))
+variables = model.init(jax.random.PRNGKey(0), batch)
+# print(variables)
+# for _ in range(4000):
+#     output = 
---
+++ cleanrl/ddpg_continuous_action_jax.py
@@ -0,0 +1,287 @@
+# docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/ddpg/#ddpg_continuous_actionpy
+# docs and experiment results can be found at
+# https://docs.cleanrl.dev/rl-algorithms/ddpg/#ddpg_continuous_actionpy
+
+import argparse
+import os
+import random
+import time
+from distutils.util import strtobool
+from typing import Sequence
+
+import gym
+import numpy as np
+import pybullet_envs  # noqa
+import torch
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+import optax
+import torch.nn.functional as F
+import torch.optim as optim
+from stable_baselines3.common.buffers import ReplayBuffer
+from torch.utils.tensorboard import SummaryWriter
+
+
+def parse_args():
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
+        help="the name of this experiment")
+    parser.add_argument("--seed", type=int, default=1,
+        help="seed of the experiment")
+    parser.add_argument("--torch-deterministic", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="if toggled, `torch.backends.cudnn.deterministic=False`")
+    parser.add_argument("--cuda", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="if toggled, cuda will be enabled by default")
+    parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="if toggled, this experiment will be tracked with Weights and Biases")
+    parser.add_argument("--wandb-project-name", type=str, default="cleanRL",
+        help="the wandb's project name")
+    parser.add_argument("--wandb-entity", type=str, default=None,
+        help="the entity (team) of wandb's project")
+    parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="weather to capture videos of the agent performances (check out `videos` folder)")
+
+    # Algorithm specific arguments
+    parser.add_argument("--env-id", type=str, default="HalfCheetah-v2",
+        help="the id of the environment")
+    parser.add_argument("--total-timesteps", type=int, default=1000000,
+        help="total timesteps of the experiments")
+    parser.add_argument("--learning-rate", type=float, default=3e-4,
+        help="the learning rate of the optimizer")
+    parser.add_argument("--buffer-size", type=int, default=int(1e6),
+        help="the replay memory buffer size")
+    parser.add_argument("--gamma", type=float, default=0.99,
+        help="the discount factor gamma")
+    parser.add_argument("--tau", type=float, default=0.005,
+        help="target smoothing coefficient (default: 0.005)")
+    parser.add_argument("--batch-size", type=int, default=256,
+        help="the batch size of sample from the reply memory")
+    parser.add_argument("--exploration-noise", type=float, default=0.1,
+        help="the scale of exploration noise")
+    parser.add_argument("--learning-starts", type=int, default=25e3,
+        help="timestep to start learning")
+    parser.add_argument("--policy-frequency", type=int, default=2,
+        help="the frequency of training policy (delayed)")
+    parser.add_argument("--noise-clip", type=float, default=0.5,
+        help="noise clip parameter of the Target Policy Smoothing Regularization")
+    args = parser.parse_args()
+    # fmt: on
+    return args
+
+
+def make_env(env_id, seed, idx, capture_video, run_name):
+    def thunk():
+        env = gym.make(env_id)
+        # env = gym.wrappers.RecordEpisodeStatistics(env)
+        if capture_video:
+            if idx == 0:
+                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        env.seed(seed)
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+        return env
+
+    return thunk
+
+
+# ALGO LOGIC: initialize agent here:
+class QNetwork(nn.Module):
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Dense(256)(x)
+        x = nn.relu(x)
+        x = nn.Dense(256)(x)
+        x = nn.relu(x)
+        x = nn.Dense(256)(x)
+        x = nn.relu(x)
+        return x
+
+class Actor(nn.Module):
+    # state_dim = None
+    action_dim: Sequence[int]
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Dense(256)(x)
+        x = nn.relu(x)
+        x = nn.Dense(256)(x)
+        x = nn.relu(x)
+        x = nn.Dense(self.action_dim)(x)
+        return x
+
+
+# class QNetwork(nn.Module):
+#     def __init__(self, env):
+#         super().__init__()
+#         self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod() + np.prod(env.single_action_space.shape), 256)
+#         self.fc2 = nn.Linear(256, 256)
+#         self.fc3 = nn.Linear(256, 1)
+
+#     def forward(self, x, a):
+#         x = torch.cat([x, a], 1)
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return x
+
+
+# class Actor(nn.Module):
+#     def __init__(self, env):
+#         super().__init__()
+#         self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod(), 256)
+#         self.fc2 = nn.Linear(256, 256)
+#         self.fc_mu = nn.Linear(256, np.prod(env.single_action_space.shape))
+
+#     def forward(self, x):
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         return torch.tanh(self.fc_mu(x))
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        import wandb
+
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    # writer = SummaryWriter(f"runs/{run_name}")
+    # writer.add_text(
+    #     "hyperparameters",
+    #     "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    # )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+    jaxRNG = jax.random.PRNGKey(0)
+
+
+    # device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+
+    # env setup
+    # envs = gym.vector.SyncVectorEnv([make_env(args.env_id, args.seed, 0, args.capture_video, run_name)])
+    env = gym.make(args.env_id)
+    # assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
+
+    max_action = float(env.action_space.high[0])
+
+    # actor = Actor(envs).to(device)
+
+    # envs.single_observation_space.dtype = np.float32
+    # rb = ReplayBuffer(args.buffer_size, envs.single_observation_space, envs.single_action_space, device="cpu")
+    start_time = time.time()
+
+    # TRY NOT TO MODIFY: start the game
+    obs = env.reset()
+
+
+
+
+    actor = Actor(action_dim=np.prod(env.action_space.shape))
+    actor_parameters = actor.init(jaxRNG, obs)
+    actor_sample_fn = jax.jit(actor.apply)
+    # 
+    # print(output)
+    # qf1 = QNetwork(envs).to(device)
+    # qf1_target = QNetwork(envs).to(device)
+    # target_actor = Actor(envs).to(device)
+    # target_actor.load_state_dict(actor.state_dict())
+    # qf1_target.load_state_dict(qf1.state_dict())
+    # q_optimizer = optim.Adam(list(qf1.parameters()), lr=args.learning_rate)
+    actor_optimizer = optax.adam(learning_rate=args.learning_rate)
+    actor_optimizer_state = actor_optimizer.init(actor_parameters)
+
+    # raise
+    for global_step in range(args.total_timesteps):
+        # ALGO LOGIC: put action logic here
+        if global_step < args.learning_starts:
+            actions = env.action_space.sample()
+        else:
+            actions = actor_sample_fn(actor_parameters, obs)
+            # actions = np.array(
+            #     [
+            #         (
+            #             actions.tolist()[0]
+            #             + np.random.normal(0, max_action * args.exploration_noise, size=envs.single_action_space.shape[0])
+            #         ).clip(envs.single_action_space.low, envs.single_action_space.high)
+            #     ]
+            # )
+
+        # TRY NOT TO MODIFY: execute the game and log data.
+        next_obs, rewards, dones, infos = env.step(actions)
+        if dones:
+            next_obs = env.reset()
+
+        # TRY NOT TO MODIFY: record rewards for plotting purposes
+        # for info in infos:
+        #     if "episode" in info.keys():
+        #         print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
+        #         writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+        #         writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
+        #         break
+
+        # TRY NOT TO MODIFY: save data to reply buffer; handle `terminal_observation`
+        # real_next_obs = next_obs.copy()
+        # for idx, d in enumerate(dones):
+        #     if d:
+        #         real_next_obs[idx] = infos[idx]["terminal_observation"]
+        # rb.add(obs, real_next_obs, actions, rewards, dones, infos)
+
+        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
+        obs = next_obs
+
+        if global_step % 10000 == 0:
+            print("SPS:", int(global_step / (time.time() - start_time)))
+            # writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+
+        # ALGO LOGIC: training.
+        # if global_step > args.learning_starts:
+            # data = rb.sample(args.batch_size)
+            # with torch.no_grad():
+            #     next_state_actions = (target_actor(data.next_observations)).clamp(
+            #         envs.single_action_space.low[0], envs.single_action_space.high[0]
+            #     )
+            #     qf1_next_target = qf1_target(data.next_observations, next_state_actions)
+            #     next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (qf1_next_target).view(-1)
+
+            # qf1_a_values = qf1(data.observations, data.actions).view(-1)
+            # qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
+
+            # # optimize the model
+            # q_optimizer.zero_grad()
+            # qf1_loss.backward()
+            # q_optimizer.step()
+
+            # if global_step % args.policy_frequency == 0:
+            #     actor_loss = -qf1(data.observations, actor(data.observations)).mean()
+            #     actor_optimizer.zero_grad()
+            #     actor_loss.backward()
+            #     actor_optimizer.step()
+
+            #     # update the target network
+            #     for param, target_param in zip(actor.parameters(), target_actor.parameters()):
+            #         target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+            #     for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
+            #         target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+
+            # if global_step % 10000 == 0:
+            #     # writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
+            #     # writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
+            #     # writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
+            #     print("SPS:", int(global_step / (time.time() - start_time)))
+            #     writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+
+    envs.close()
+    # writer.close()
