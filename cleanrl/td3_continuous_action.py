9985d630ec8ab00f7fa966d025171c77c0051945
dosssman <dosssman@hotmail.fr>
2022-06-26 01:23:20 +0900

    TD3 adjusted the exploration noise for the policy during the rollout based on action_bias and action_scale

--- cleanrl/td3_continuous_action.py
+++ cleanrl/td3_continuous_action.py
@@ -182,7 +182,7 @@ if __name__ == "__main__":
                 [
                     (
                         actions.tolist()[0]
-                        + np.random.normal(0, max_action * args.exploration_noise, size=envs.single_action_space.shape[0])
+                        + np.random.normal(actor.action_bias[0].cpu().numpy(), actor.action_scale[0].cpu().numpy() * args.exploration_noise, size=envs.single_action_space.shape[0])
                     ).clip(envs.single_action_space.low, envs.single_action_space.high)
                 ]
             )
