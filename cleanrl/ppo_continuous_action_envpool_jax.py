c769efcec42b624534cf2508a64872b1ce62af18
Costa Huang <costa.huang@outlook.com>
2022-06-26 12:17:28 -0400

    next step

--- cleanrl/ppo_continuous_action_envpool_jax.py
+++ cleanrl/ppo_continuous_action_envpool_jax.py
@@ -7,7 +7,10 @@ from collections import deque
 from distutils.util import strtobool
 from typing import Sequence
 
+from cv2 import log
+
 import envpool
+import flax
 import flax.linen as nn
 import gym
 import jax
@@ -42,11 +45,11 @@ def parse_args():
         help="the id of the environment")
     parser.add_argument("--total-timesteps", type=int, default=10000000,
         help="total timesteps of the experiments")
-    parser.add_argument("--learning-rate", type=float, default=3e-4,
+    parser.add_argument("--learning-rate", type=float, default=0.00295,
         help="the learning rate of the optimizer")
     parser.add_argument("--num-envs", type=int, default=64,
         help="the number of parallel game environments")
-    parser.add_argument("--num-steps", type=int, default=128,
+    parser.add_argument("--num-steps", type=int, default=64,
         help="the number of steps to run in each environment per policy rollout")
     parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggle learning rate annealing for policy and value networks")
@@ -56,21 +59,21 @@ def parse_args():
         help="the discount factor gamma")
     parser.add_argument("--gae-lambda", type=float, default=0.95,
         help="the lambda for the general advantage estimation")
-    parser.add_argument("--num-minibatches", type=int, default=2,
+    parser.add_argument("--num-minibatches", type=int, default=4,
         help="the number of mini-batches")
-    parser.add_argument("--update-epochs", type=int, default=4,
+    parser.add_argument("--update-epochs", type=int, default=2,
         help="the K epochs to update the policy")
     parser.add_argument("--norm-adv", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggles advantages normalization")
-    parser.add_argument("--clip-coef", type=float, default=0.1,
+    parser.add_argument("--clip-coef", type=float, default=0.2,
         help="the surrogate clipping coefficient")
-    parser.add_argument("--clip-vloss", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+    parser.add_argument("--clip-vloss", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="Toggles whether or not to use a clipped loss for the value function, as per the paper.")
-    parser.add_argument("--ent-coef", type=float, default=0.01,
+    parser.add_argument("--ent-coef", type=float, default=0.0,
         help="coefficient of the entropy")
-    parser.add_argument("--vf-coef", type=float, default=0.5,
+    parser.add_argument("--vf-coef", type=float, default=1.3,
         help="coefficient of the value function")
-    parser.add_argument("--max-grad-norm", type=float, default=0.5,
+    parser.add_argument("--max-grad-norm", type=float, default=3.5,
         help="the maximum norm for the gradient clipping")
     parser.add_argument("--target-kl", type=float, default=None,
         help="the target KL divergence threshold")
@@ -133,7 +136,6 @@ class Critic(nn.Module):
 
 class Actor(nn.Module):
     action_dim: Sequence[int]
-
     @nn.compact
     def __call__(self, x):
         actor_mean = nn.Dense(64)(x)
@@ -143,6 +145,11 @@ class Actor(nn.Module):
         actor_mean = nn.Dense(self.action_dim)(actor_mean)
         actor_logstd = jnp.zeros((1, self.action_dim))
         return actor_mean, actor_logstd
+
+@flax.struct.dataclass
+class AgentParams:
+  actor_params: flax.core.FrozenDict
+  critic_params: flax.core.FrozenDict
 
 
 if __name__ == "__main__":
@@ -169,7 +176,8 @@ if __name__ == "__main__":
     # TRY NOT TO MODIFY: seeding
     random.seed(args.seed)
     np.random.seed(args.seed)
-    jaxRNG = jax.random.PRNGKey(args.seed)
+    key = jax.random.PRNGKey(args.seed)
+    key, actor_key, critic_key = jax.random.split(key, 3)
 
     # env setup
     envs = envpool.make(
@@ -190,13 +198,17 @@ if __name__ == "__main__":
     assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
 
     actor = Actor(action_dim=np.prod(envs.single_action_space.shape))
-    actor_parameters = actor.init(jaxRNG, envs.single_observation_space.sample())
+    actor_params = actor.init(actor_key, envs.single_observation_space.sample())
     actor.apply = jax.jit(actor.apply)
     critic = Critic()
-    critic_parameters = critic.init(jaxRNG, envs.single_observation_space.sample())
+    critic_params = critic.init(critic_key, envs.single_observation_space.sample())
     critic.apply = jax.jit(critic.apply)
-    actor_optimizer = optax.adam(learning_rate=args.learning_rate, eps=1e-5)
-    actor_optimizer_state = actor_optimizer.init((actor_parameters, critic_parameters))
+    agent_optimizer = optax.adam(learning_rate=args.learning_rate, eps=1e-5)
+    agent_params = AgentParams(
+        actor_params,
+        critic_params,
+    )
+    agent_optimizer_state = agent_optimizer.init(agent_params)
 
     # ALGO Logic: Storage setup
     obs = jnp.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape)
@@ -205,21 +217,106 @@ if __name__ == "__main__":
     rewards = jnp.zeros((args.num_steps, args.num_envs))
     dones = jnp.zeros((args.num_steps, args.num_envs))
     values = jnp.zeros((args.num_steps, args.num_envs))
+    advantages = jnp.zeros((args.num_steps, args.num_envs))
     avg_returns = deque(maxlen=20)
 
     @jax.jit
-    def get_action_and_value(x, obs, actions, logprobs, values, step, jaxRNG, actor_parameters, critic_parameters):
+    def get_action_and_value(x, obs, actions, logprobs, values, step, agent_params, key):
         obs = obs.at[step].set(x)  # inside jit() `x = x.at[idx].set(y)` is in-place.
-        action_mean, action_logstd = actor.apply(actor_parameters, x)
+        action_mean, action_logstd = actor.apply(agent_params.actor_params, x)
         action_std = jnp.exp(action_logstd)
-        action = action_mean + action_std * jax.random.normal(jaxRNG, shape=action_mean.shape)
+        key, subkey = jax.random.split(key)
+        action = action_mean + action_std * jax.random.normal(subkey, shape=action_mean.shape)
         logprob = -0.5 * ((action - action_mean) / action_std) ** 2 - 0.5 * jnp.log(2.0 * jnp.pi) - action_logstd
         entropy = action_logstd + 0.5 * jnp.log(2.0 * jnp.pi * jnp.e)
-        value = critic.apply(critic_parameters, x)
+        value = critic.apply(agent_params.critic_params, x)
         actions = actions.at[step].set(action)
         logprobs = logprobs.at[step].set(logprob.sum(1))
         values = values.at[step].set(value.squeeze())
-        return x, obs, actions, logprobs, values, action, logprob, entropy, value
+        return x, obs, actions, logprobs, values, action, logprob, entropy, value, key
+
+    @jax.jit
+    def get_action_and_value2(x, action, agent_params):
+        action_mean, action_logstd = actor.apply(agent_params.actor_params, x)
+        action_std = jnp.exp(action_logstd)
+        logprob = -0.5 * ((action - action_mean) / action_std) ** 2 - 0.5 * jnp.log(2.0 * jnp.pi) - action_logstd
+        entropy = action_logstd + 0.5 * jnp.log(2.0 * jnp.pi * jnp.e)
+        value = critic.apply(agent_params.critic_params, x)
+        return logprob.sum(1), entropy, value
+
+    @jax.jit
+    def compute_gae(next_obs, next_done, rewards, dones, values, advantages, agent_params):
+        advantages = advantages.at[:].set(0.0) # reset advantages
+        next_value = critic.apply(agent_params.critic_params, next_obs).squeeze()
+        lastgaelam = 0
+        for t in reversed(range(args.num_steps)):
+            if t == args.num_steps - 1:
+                nextnonterminal = 1.0 - next_done
+                nextvalues = next_value
+            else:
+                nextnonterminal = 1.0 - dones[t + 1]
+                nextvalues = values[t + 1]
+            delta = rewards[t] + args.gamma * nextvalues * nextnonterminal - values[t]
+            lastgaelam = delta + args.gamma * args.gae_lambda * nextnonterminal * lastgaelam
+            advantages = advantages.at[t].set(lastgaelam)
+        returns = advantages + values
+        return jax.lax.stop_gradient(advantages), jax.lax.stop_gradient(returns)
+
+    @jax.jit
+    def update_ppo(
+        obs,
+        logprobs,
+        actions,
+        advantages,
+        returns,
+        values,
+        agent_params,
+        agent_optimizer_state,
+        key
+    ):
+        b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
+        b_logprobs = logprobs.reshape(-1)
+        b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
+        b_advantages = advantages.reshape(-1)
+        b_returns = returns.reshape(-1)
+        b_values = values.reshape(-1)
+
+        def ppo_loss(agent_params, x, a, logp, adv, ret):
+            newlogprob, _, newvalue = get_action_and_value2(x, a, agent_params)
+            logratio = newlogprob - logp
+            ratio = jnp.exp(logratio)
+
+            mb_advantages = adv
+            # if args.norm_adv:
+            #     mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
+
+            # Policy loss
+            pg_loss1 = -mb_advantages * ratio
+            pg_loss2 = -mb_advantages * jnp.clip(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
+            pg_loss = jnp.maximum(pg_loss1, pg_loss2).mean()
+
+            # Value loss
+            v_loss = 0.5 * ((newvalue - ret) ** 2).mean()
+
+            # entropy_loss = entropy.mean()
+            # loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
+            loss = pg_loss + v_loss * args.vf_coef
+            return loss
+        ppo_loss_grad_fn = jax.value_and_grad(ppo_loss)
+
+        b_inds = jnp.arange(args.batch_size)
+        # clipfracs = []
+        for _ in range(args.update_epochs):
+            key, subkey = jax.random.split(key)
+            b_inds = jax.random.shuffle(subkey, b_inds)
+            for start in range(0, args.batch_size, args.minibatch_size):
+                end = start + args.minibatch_size
+                mb_inds = b_inds[start:end]
+                loss, grads = ppo_loss_grad_fn(agent_params, b_obs[mb_inds], b_actions[mb_inds], b_logprobs[mb_inds], b_advantages[mb_inds], b_returns[mb_inds])
+                updates, agent_optimizer_state = agent_optimizer.update(grads, agent_optimizer_state)
+                agent_params = optax.apply_updates(agent_params, updates)
+        
+        return loss, key, agent_params, agent_optimizer_state
 
     # TRY NOT TO MODIFY: start the game
     global_step = 0
@@ -237,8 +334,8 @@ if __name__ == "__main__":
 
         for step in range(0, args.num_steps):
             global_step += 1 * args.num_envs
-            next_obs, obs, actions, logprobs, values, action, logprob, entropy, value = get_action_and_value(
-                next_obs, obs, actions, logprobs, values, step, jaxRNG, actor_parameters, critic_parameters
+            next_obs, obs, actions, logprobs, values, action, logprob, entropy, value, key = get_action_and_value(
+                next_obs, obs, actions, logprobs, values, step, agent_params, key
             )
 
             # TRY NOT TO MODIFY: execute the game and log data.
@@ -251,100 +348,23 @@ if __name__ == "__main__":
                     writer.add_scalar("charts/episodic_return", info["r"][idx], global_step)
                     writer.add_scalar("charts/episodic_length", info["l"][idx], global_step)
 
-        # # bootstrap value if not done
-        # with torch.no_grad():
-        #     next_value = agent.get_value(next_obs).reshape(1, -1)
-        #     if args.gae:
-        #         advantages = torch.zeros_like(rewards).to(device)
-        #         lastgaelam = 0
-        #         for t in reversed(range(args.num_steps)):
-        #             if t == args.num_steps - 1:
-        #                 nextnonterminal = 1.0 - next_done
-        #                 nextvalues = next_value
-        #             else:
-        #                 nextnonterminal = 1.0 - dones[t + 1]
-        #                 nextvalues = values[t + 1]
-        #             delta = rewards[t] + args.gamma * nextvalues * nextnonterminal - values[t]
-        #             advantages[t] = lastgaelam = delta + args.gamma * args.gae_lambda * nextnonterminal * lastgaelam
-        #         returns = advantages + values
-        #     else:
-        #         returns = torch.zeros_like(rewards).to(device)
-        #         for t in reversed(range(args.num_steps)):
-        #             if t == args.num_steps - 1:
-        #                 nextnonterminal = 1.0 - next_done
-        #                 next_return = next_value
-        #             else:
-        #                 nextnonterminal = 1.0 - dones[t + 1]
-        #                 next_return = returns[t + 1]
-        #             returns[t] = rewards[t] + args.gamma * nextnonterminal * next_return
-        #         advantages = returns - values
+        advantages, returns = compute_gae(next_obs, next_done, rewards, dones, values, advantages, agent_params)
+        # print(advantages.sum(), returns.sum())
+        # raise
+        loss, key, agent_params, agent_optimizer_state = update_ppo(
+            obs,
+            logprobs,
+            actions,
+            advantages,
+            returns,
+            values,
+            agent_params,
+            agent_optimizer_state,
+            key,
+        )
 
-        # # flatten the batch
-        # b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
-        # b_logprobs = logprobs.reshape(-1)
-        # b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
-        # b_advantages = advantages.reshape(-1)
-        # b_returns = returns.reshape(-1)
-        # b_values = values.reshape(-1)
+        print(agent_params.actor_params["params"]["Dense_0"]["kernel"].sum(), agent_params.critic_params["params"]["Dense_0"]["kernel"].sum())
 
-        # # Optimizing the policy and value network
-        # b_inds = np.arange(args.batch_size)
-        # clipfracs = []
-        # for epoch in range(args.update_epochs):
-        #     np.random.shuffle(b_inds)
-        #     for start in range(0, args.batch_size, args.minibatch_size):
-        #         end = start + args.minibatch_size
-        #         mb_inds = b_inds[start:end]
-
-        #         _, newlogprob, entropy, newvalue = agent.get_action_and_value(b_obs[mb_inds], b_actions[mb_inds])
-        #         logratio = newlogprob - b_logprobs[mb_inds]
-        #         ratio = logratio.exp()
-
-        #         with torch.no_grad():
-        #             # calculate approx_kl http://joschu.net/blog/kl-approx.html
-        #             old_approx_kl = (-logratio).mean()
-        #             approx_kl = ((ratio - 1) - logratio).mean()
-        #             clipfracs += [((ratio - 1.0).abs() > args.clip_coef).float().mean().item()]
-
-        #         mb_advantages = b_advantages[mb_inds]
-        #         if args.norm_adv:
-        #             mb_advantages = (mb_advantages - mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
-
-        #         # Policy loss
-        #         pg_loss1 = -mb_advantages * ratio
-        #         pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
-        #         pg_loss = torch.max(pg_loss1, pg_loss2).mean()
-
-        #         # Value loss
-        #         newvalue = newvalue.view(-1)
-        #         if args.clip_vloss:
-        #             v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
-        #             v_clipped = b_values[mb_inds] + torch.clamp(
-        #                 newvalue - b_values[mb_inds],
-        #                 -args.clip_coef,
-        #                 args.clip_coef,
-        #             )
-        #             v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
-        #             v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
-        #             v_loss = 0.5 * v_loss_max.mean()
-        #         else:
-        #             v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
-
-        #         entropy_loss = entropy.mean()
-        #         loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
-
-        #         optimizer.zero_grad()
-        #         loss.backward()
-        #         nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
-        #         optimizer.step()
-
-        #     if args.target_kl is not None:
-        #         if approx_kl > args.target_kl:
-        #             break
-
-        # y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
-        # var_y = np.var(y_true)
-        # explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
 
         # # TRY NOT TO MODIFY: record rewards for plotting purposes
         # writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
@@ -354,7 +374,7 @@ if __name__ == "__main__":
         # writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
         # writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
         # writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
-        # writer.add_scalar("losses/explained_variance", explained_var, global_step)
+        writer.add_scalar("losses/loss", loss.item(), global_step)
         print("SPS:", int(global_step / (time.time() - start_time)))
         writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
 
