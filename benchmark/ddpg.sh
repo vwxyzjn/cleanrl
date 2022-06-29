9ec4ac544e89332b0217e13a18e424d40215d196
Costa Huang <costa.huang@outlook.com>
2022-06-28 20:49:35 -0400

    update docs

--- benchmark/ddpg.sh
+++ benchmark/ddpg.sh
@@ -1,7 +1,16 @@
 poetry install -E "mujoco pybullet"
-python -c "import mujoco_py"
+poetry run python -c "import mujoco_py"
 OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
     --env-ids HalfCheetah-v2 Walker2d-v2 Hopper-v2 \
     --command "poetry run python cleanrl/ddpg_continuous_action.py --track --capture-video" \
     --num-seeds 3 \
     --workers 3
+
+poetry install -E "mujoco jax"
+poetry run pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+poetry run python -c "import mujoco_py"
+OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
+    --env-ids HalfCheetah-v2 Walker2d-v2 Hopper-v2 \
+    --command "poetry run python cleanrl/ddpg_continuous_action_jax.py --track --capture-video" \
+    --num-seeds 3 \
+    --workers 1
--- benchmark/ppo.sh
+++ benchmark/ppo.sh
@@ -29,7 +29,7 @@ xvfb-run -a python -m cleanrl_utils.benchmark \
     --workers 1
 
 poetry install -E "mujoco pybullet"
-python -c "import mujoco_py"
+poetry run python -c "import mujoco_py"
 OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
     --env-ids HalfCheetah-v2 Walker2d-v2 Hopper-v2 \
     --command "poetry run python cleanrl/ppo_continuous_action.py --cuda False --track --capture-video" \
