15df5c0eeb3459cc5b3dadd722751a80261c4a5e
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2022-06-30 22:02:34 +0900

    Td3 ddpg action bound fix (#211)
    * Prototype the actor's output action scaling to the action space of the environment
    
    * TD3 and DDPG's action scale and bias move to GPU if needs be
    
    * Fixed formatting
    
    * pre-commit fixed formating
    
    * action_scale and action_bias in TD3 / DDPG and SAC finally using register_buffer
    
    * Fixed the = self.register_buffer code artifact
    
    * TD3 adjusted the exploration noise for the policy during the rollout based on action_bias and action_scale
    
    * Removed obsolete next obs actions clamping
    
    * td3 format fixed by pre-cmmit
    
    * cosmatic change: make `handle_timeout_termination` explicit
    
    * Quick fix
    
    * update docs
    
    * Update benchmark
    
    * update docs
    
    * Update docs
    
    * DDPG and TD3: got rid of max_action, exploration noise is sampled from distribution that is centered at the mena of the action space boundaries
    
    * Update docs
    
    * Updated TD3 and DDPG regarding the action_mean and action_scale usage
    
    * Reduced needless device copy when sampling action for follouts in DDPG and TD3
    
    * Quick fix
    
    Co-authored-by: Costa Huang <costa.huang@outlook.com>

--- cleanrl/td3_continuous_action.py
+++ cleanrl/td3_continuous_action.py
@@ -103,11 +103,15 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod(), 256)
         self.fc2 = nn.Linear(256, 256)
         self.fc_mu = nn.Linear(256, np.prod(env.single_action_space.shape))
+        # action rescaling
+        self.register_buffer("action_scale", torch.FloatTensor((env.action_space.high - env.action_space.low) / 2.0))
+        self.register_buffer("action_bias", torch.FloatTensor((env.action_space.high + env.action_space.low) / 2.0))
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        return torch.tanh(self.fc_mu(x))
+        x = torch.tanh(self.fc_mu(x))
+        return x * self.action_scale + self.action_bias
 
 
 if __name__ == "__main__":
@@ -143,7 +147,6 @@ if __name__ == "__main__":
     envs = gym.vector.SyncVectorEnv([make_env(args.env_id, args.seed, 0, args.capture_video, run_name)])
     assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
 
-    max_action = float(envs.single_action_space.high[0])
     actor = Actor(envs).to(device)
     qf1 = QNetwork(envs).to(device)
     qf2 = QNetwork(envs).to(device)
@@ -173,15 +176,10 @@ if __name__ == "__main__":
         if global_step < args.learning_starts:
             actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
         else:
-            actions = actor(torch.Tensor(obs).to(device))
-            actions = np.array(
-                [
-                    (
-                        actions.tolist()[0]
-                        + np.random.normal(0, max_action * args.exploration_noise, size=envs.single_action_space.shape[0])
-                    ).clip(envs.single_action_space.low, envs.single_action_space.high)
-                ]
-            )
+            with torch.no_grad():
+                actions = actor(torch.Tensor(obs).to(device))
+                actions += torch.normal(actor.action_bias, actor.action_scale * args.exploration_noise)
+                actions = actions.cpu().numpy().clip(envs.single_action_space.low, envs.single_action_space.high)
 
         # TRY NOT TO MODIFY: execute the game and log data.
         next_obs, rewards, dones, infos = envs.step(actions)
