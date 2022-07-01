bug fix target_pmfs calculation and adapt to use TrainState API

cleanrl/c51_jax.py
@@ -5,12 +5,14 @@ import random
 import time
 from distutils.util import strtobool
 
+import flax
 import flax.linen as nn
 import gym
 import jax
 import jax.numpy as jnp
 import numpy as np
 import optax
+from flax.training.train_state import TrainState
 from stable_baselines3.common.buffers import ReplayBuffer
 from torch.utils.tensorboard import SummaryWriter
 
@@ -105,6 +107,11 @@ class QNetwork(nn.Module):
         return x
 
 
+class TrainState(TrainState):
+    target_params: flax.core.FrozenDict
+    atoms: jnp.ndarray
+
+
 def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
     slope = (end_e - start_e) / duration
     return max(slope * t + start_e, end_e)
@@ -144,15 +151,19 @@ if __name__ == "__main__":
     obs = envs.reset()
 
     q_network = QNetwork(action_dim=envs.single_action_space.n, n_atoms=args.n_atoms, v_min=args.v_min, v_max=args.v_max)
-    q_params = q_network.init(q_key, obs)
-    target_params = q_network.init(q_key, obs)
-    q_network.apply = jax.jit(q_network.apply)
-    target_params = optax.incremental_update(q_params, target_params, step_size=1)
-    # directly using jnp.linspace leads to numerical errors
-    atoms = jnp.asarray(np.linspace(args.v_min, args.v_max, num=args.n_atoms))
 
-    optimizer = optax.adam(learning_rate=args.learning_rate)
-    optimizer_state = optimizer.init(q_params)
+    q_state = TrainState.create(
+        apply_fn=q_network.apply,
+        params=q_network.init(q_key, obs),
+        target_params=q_network.init(q_key, obs),
+        # directly using jnp.linspace leads to numerical errors
+        atoms = jnp.asarray(np.linspace(args.v_min, args.v_max, num=args.n_atoms)),
+        tx=optax.adam(learning_rate=args.learning_rate),
+    )
+
+    q_network.apply = jax.jit(q_network.apply)
+    # This step is not necessary as init called on same observation and key will always lead to same initializations
+    q_state = q_state.replace(target_params=optax.incremental_update(q_state.params, q_state.target_params, 1))
 
     rb = ReplayBuffer(
         args.buffer_size,
@@ -162,46 +173,41 @@ if __name__ == "__main__":
         handle_timeout_termination=True,
     )
 
-    # def get_action_pmfs(params, obs):
-    #     pmfs = q_network(params, obs)  # (batch_size, num_actions, num_atoms)
-    #     q_values = (atoms * pmfs).sum(axis=-1)  # (batch_size, num_actions)
-    #     actions = jnp.argmax(q_values, axis=-1)  # (batch_size,)
-
     @jax.jit
-    def update(observations, actions, next_observations, rewards, dones, q_params, target_params, optimizer_params):
-        next_pmfs = q_network.apply(target_params, next_observations)  # (batch_size, num_actions, num_atoms)
-        next_vals = (next_pmfs * atoms).sum(axis=-1)  # (batch_size, num_actions)
+    def update(q_state, observations, actions, next_observations, rewards, dones):
+        next_pmfs = q_network.apply(q_state.target_params, next_observations)  # (batch_size, num_actions, num_atoms)
+        next_vals = (next_pmfs * q_state.atoms).sum(axis=-1)  # (batch_size, num_actions)
         next_action = jnp.argmax(next_vals, axis=-1)  # (batch_size,)
         next_pmfs = next_pmfs[np.arange(next_pmfs.shape[0]), next_action]
-        next_atoms = rewards + args.gamma * atoms * (1 - dones)
+        next_atoms = rewards + args.gamma * q_state.atoms * (1 - dones)
         # projection
-        delta_z = atoms[1] - atoms[0]
-        tz = jax.lax.clamp(jnp.float32(args.v_min), next_atoms, jnp.float32(args.v_max))
+        delta_z = q_state.atoms[1] - q_state.atoms[0]
+        tz = jnp.clip(next_atoms, a_min=(args.v_min), a_max=(args.v_max))
+        
         b = (tz - args.v_min) / delta_z
-        l = jax.lax.clamp(0.0, jnp.floor(b), args.n_atoms - 1.0)
-        u = jax.lax.clamp(0.0, jnp.ceil(b), args.n_atoms - 1.0)
+        l = jnp.clip(jnp.floor(b), a_min=0, a_max=args.n_atoms - 1)
+        u = jnp.clip(jnp.ceil(b), a_min=0, a_max=args.n_atoms - 1)
         # (l == u).astype(jnp.float) handles the case where bj is exactly an integer
         # example bj = 1, then the upper ceiling should be uj= 2, and lj= 1
         d_m_l = (u + (l == u).astype(jnp.float32) - b) * next_pmfs
         d_m_u = (b - l) * next_pmfs
-
         target_pmfs = jnp.zeros_like(next_pmfs)
+
         for i in range(target_pmfs.shape[0]):
-            target_pmfs = target_pmfs.at[l[i].astype(jnp.int32)].add(d_m_l[i])
-            target_pmfs = target_pmfs.at[u[i].astype(jnp.int32)].add(d_m_u[i])
+            target_pmfs = target_pmfs.at[i,l[i].astype(jnp.int32)].add(d_m_l[i])
+            target_pmfs = target_pmfs.at[i,u[i].astype(jnp.int32)].add(d_m_u[i])
 
         def loss(q_params, observations, actions, target_pmfs):
             pmfs = q_network.apply(q_params, observations)
             old_pmfs = pmfs[np.arange(pmfs.shape[0]), actions.squeeze()]
-            # target_params = jnp.clip(target_pmfs, a_min=1e-5, a_max=1 - 1e-5)
+
             old_pmfs_l = jnp.clip(old_pmfs, a_min=1e-5, a_max=1 - 1e-5)
             loss = (-(target_pmfs * jnp.log(old_pmfs_l)).sum(-1)).mean()
-            return loss, (old_pmfs * atoms).sum(-1)
+            return loss, (old_pmfs * q_state.atoms).sum(-1)
 
-        (loss_value, old_values), grads = jax.value_and_grad(loss, has_aux=True)(q_params, observations, actions, target_pmfs)
-        updates, optimizer_params = optimizer.update(grads, optimizer_params)
-        q_params = optax.apply_updates(q_params, updates)
-        return loss_value, old_values, q_params, optimizer_params
+        (loss_value, old_values), grads = jax.value_and_grad(loss, has_aux=True)(q_state.params, observations, actions, target_pmfs)
+        q_state = q_state.apply_gradients(grads=grads)
+        return loss_value, old_values, q_state
 
     start_time = time.time()
 
@@ -213,8 +219,8 @@ if __name__ == "__main__":
         if random.random() < epsilon:
             actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
         else:
-            pmfs = q_network.apply(q_params, obs)
-            q_vals = (pmfs * atoms).sum(axis=-1)
+            pmfs = q_network.apply(q_state.params, obs)
+            q_vals = (pmfs * q_state.atoms).sum(axis=-1)
             actions = q_vals.argmax(axis=-1)
             actions = jax.device_get(actions)
 
@@ -243,15 +249,13 @@ if __name__ == "__main__":
         # ALGO LOGIC: training.
         if global_step > args.learning_starts and global_step % args.train_frequency == 0:
             data = rb.sample(args.batch_size)
-            loss, old_val, q_params, optimizer_params = update(
+            loss, old_val, q_state = update(
+                q_state,
                 data.observations.numpy(),
                 data.actions.numpy(),
                 data.next_observations.numpy(),
                 data.rewards.numpy(),
                 data.dones.numpy(),
-                q_params,
-                target_params,
-                optimizer_state,
             )
 
             if global_step % 100 == 0:
@@ -262,7 +266,7 @@ if __name__ == "__main__":
 
             # update the target network
             if global_step % args.target_network_frequency == 0:
-                target_params = optax.incremental_update(q_params, target_params, 1)
+                q_state = q_state.replace(target_params=optax.incremental_update(q_state.params, q_state.target_params, 1))
 
     envs.close()
     writer.close()