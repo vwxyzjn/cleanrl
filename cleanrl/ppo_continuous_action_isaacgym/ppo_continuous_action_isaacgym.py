be9af7a63186774b700f627f9e2119c4757714e1
Costa Huang <costa.huang@outlook.com>
2022-07-25 10:23:13 -0400

    Remove the github pages CI in favor of vercel (#241)
    * Remove the github pages CI in favor of vercel
    
    * update docs
    
    * update PR template

--- cleanrl/ppo_continuous_action_isaacgym/ppo_continuous_action_isaacgym.py
+++ cleanrl/ppo_continuous_action_isaacgym/ppo_continuous_action_isaacgym.py
@@ -26,6 +26,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/ppo/#ppo_continuous_action_isaacgympy
 import argparse
 import os
 import random
