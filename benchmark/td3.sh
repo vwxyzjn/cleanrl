5e5647a4d279a931a5d3c010edbf0c06c65768c0
Costa Huang <costa.huang@outlook.com>
2022-07-27 17:43:07 -0400

    update docs

--- benchmark/td3.sh
+++ benchmark/td3.sh
@@ -5,3 +5,12 @@ OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
     --command "poetry run python cleanrl/td3_continuous_action.py --track --capture-video" \
     --num-seeds 3 \
     --workers 3
+
+poetry install -E "mujoco jax"
+poetry run pip install --upgrade "jax[cuda]==0.3.14" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+poetry run python -c "import mujoco_py"
+OMP_NUM_THREADS=1 xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
+    --env-ids HalfCheetah-v2 Walker2d-v2 Hopper-v2 \
+    --command "poetry run python cleanrl/td3_continuous_action_jax.py --track --capture-video" \
+    --num-seeds 3 \
+    --workers 1
