e77e789f102c95c5399985c09ee423921056ca4b
Costa Huang <costa.huang@outlook.com>
2022-07-28 12:01:28 -0400

    fix pre-commit

--- tests/test_atari.py
+++ tests/test_atari.py
@@ -24,6 +24,7 @@ def test_dqn():
         check=True,
     )
 
+
 def test_c51():
     subprocess.run(
         "python cleanrl/c51_atari.py --learning-starts 10 --total-timesteps 16 --buffer-size 10 --batch-size 4",
