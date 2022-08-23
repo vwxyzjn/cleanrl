de8a8a36d0a74cd43b0d853823a3f11ee8bc0320
Costa Huang <costa.huang@outlook.com>
2022-08-23 11:21:29 -0400

    add test cases

--- tests/test_envpool.py
+++ tests/test_envpool.py
@@ -7,3 +7,10 @@ def test_ppo_envpool():
         shell=True,
         check=True,
     )
+
+def test_ppo_envpool():
+    subprocess.run(
+        "python cleanrl/ppo_rnd_envpool.py --num-envs 8 --num-steps 32 --num-iterations-obs-norm-init 1 --total-timesteps 256",
+        shell=True,
+        check=True,
+    )
