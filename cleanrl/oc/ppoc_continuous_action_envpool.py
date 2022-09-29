f9e87d6cc749a10e206fbb0412e726e3f30ac4a6
David Slayback <slayback.d@northeastern.edu>
2022-09-29 15:05:59 -0400

    Thinking

--- cleanrl/oc/ppoc_continuous_action_envpool.py
+++ cleanrl/oc/ppoc_continuous_action_envpool.py
@@ -1,6 +1,39 @@
 # Proximal Policy Option Critic (PPOC) Original implementation
 # Paper: https://arxiv.org/pdf/1712.00004.pdf
 # Reference repo: https://github.com/mklissa/PPOC
+"""Notes
+
+MLP policy in PPOC is weird:
+    1 body shared by critic and termination (but no gradient from termination)
+    1 body shared by intra- and inter-option actors (but no gradient from inter-)
+
+1 environment despite using PPO
+uses batch_size=32 (if 2 options) or 64 (if 1 option) instead of 32 minibatches
+GAE computation based on value of current option (Q(s, w))
+Observation normalization but not reward
+Per-iteration advantage normalization, no vloss clipping
+
+Consider: PG and termination loss based on s'. Q and policy loss based on s.
+    Don't resample termination and option for s' after rollout
+
+Weird tricks:
+    rewards are divided by 10 (if using options)
+    loops over options, then over epochs (so updates 1 option, then the next, then the next)
+    Only updates an option if we have at least 160 datapoints with that option (otherwise save those in a dataset, use it later)
+    Termination loss has 5e-7 learning rate (not detaul 3e-4)
+
+Execution:
+    o = env.reset()  # Start episode
+    option = pi.get_option(o)  # Start with option
+    loop:
+        a, lp_a, Q = pi.act(o, option)
+        o, r, new = env.step(a)
+        rew /= 10 (if options)
+        term = pi.get_term(o)
+        if term: option = pi.get_option(o)
+        if new: o = env.reset(); option = pi.get_option()
+        if t == T: yield accumulated sequences + bootstrap * (1 - new)
+"""
 
 import argparse
 import os
@@ -45,7 +78,7 @@ def parse_args():
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="HalfCheetah-v4",
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=1000000,
+    parser.add_argument("--total-timesteps", type=int, default=int(1e6),
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=3e-4,
         help="the learning rate of the optimizer")
@@ -55,8 +88,6 @@ def parse_args():
         help="the number of steps to run in each environment per policy rollout")
     parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggle learning rate annealing for policy and value networks")
-    parser.add_argument("--gae", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-        help="Use GAE for advantage computation")
     parser.add_argument("--gamma", type=float, default=0.99,
         help="the discount factor gamma")
     parser.add_argument("--gae-lambda", type=float, default=0.95,
@@ -69,8 +100,6 @@ def parse_args():
         help="Toggles advantages normalization")
     parser.add_argument("--clip-coef", type=float, default=0.2,
         help="the surrogate clipping coefficient")
-    parser.add_argument("--clip-vloss", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-        help="Toggles whether or not to use a clipped loss for the value function, as per the paper.")
     parser.add_argument("--ent-coef", type=float, default=0.0,
         help="coefficient of the entropy")
     parser.add_argument("--vf-coef", type=float, default=0.5,
@@ -79,6 +108,10 @@ def parse_args():
         help="the maximum norm for the gradient clipping")
     parser.add_argument("--target-kl", type=float, default=None,
         help="the target KL divergence threshold")
+    parser.add_argument("--num-options", type=int, default=2,
+        help="the number of options available")
+    parser.add_argument("--delib-cost", type=float, default=0.,
+        help="cost for switching options")
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
@@ -109,6 +142,7 @@ class StepOutput(NamedTuple):
     option_logprobs: Tensor
     logprobs: Tensor
     qs: Tensor
+    v: Tensor
 
 
 @torch.jit.script
@@ -151,7 +185,8 @@ class Agent(nn.Module):
         self.actor_logstd = nn.Parameter(torch.zeros(1, self.num_options, self.num_actions))
         self.option_actor = layer_init(nn.Linear(64, self.num_options), std=0.01)  # is detached in original implement
 
-    def step(self, x: Tensor, option_on_arrival: Tensor) -> StepOutput:
+
+    def step(self, x: Tensor, option_on_arrival: Tensor, is_done: Tensor) -> StepOutput:
         """Sample termination, then new option if needed. Pick action, return values"""
         x_qt = self.critic_body(x)
         q = self.critic(x_qt)
@@ -159,7 +194,8 @@ class Agent(nn.Module):
         term = torch.bernoulli(batched_index(option_on_arrival, beta))  # Sample termination
         x_a = self.actor_body(x)
         option_logprobs = torch.log_softmax(self.option_actor(x_a.detach()), -1)
-        option = torch.where(term > 0,
+        v = (option_logprobs.exp() * q).sum(-1)
+        option = torch.where((term + is_done) > 0,
                              torch.multinomial(option_logprobs.exp(), 1),
                              option_on_arrival)
         option_logprob = option_logprobs.gather(-1, option).squeeze(-1)
@@ -170,7 +206,7 @@ class Agent(nn.Module):
         probs = Normal(action_mean, action_std)
         action = probs.sample()
         action_logprob = probs.log_prob(action).sum(-1)
-        return StepOutput(term, option, action, beta, option_logprob, action_logprob, q)
+        return StepOutput(term, option, action, beta, option_logprob, action_logprob, q, v)
 
 
 
