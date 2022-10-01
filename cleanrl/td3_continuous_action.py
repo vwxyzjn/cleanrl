TD3: fixed dimension of clippednoise for target actions, added noise scaling based on action_scale

cleanrl/td3_continuous_action.py
@@ -206,9 +206,9 @@ if __name__ == "__main__":
         if global_step > args.learning_starts:
             data = rb.sample(args.batch_size)
             with torch.no_grad():
-                clipped_noise = (torch.randn_like(torch.Tensor(actions[0])) * args.policy_noise).clamp(
+                clipped_noise = (torch.randn_like(torch.Tensor(data.actions)) * args.policy_noise).clamp(
                     -args.noise_clip, args.noise_clip
-                )
+                ) * target_actor.action_scale
 
                 next_state_actions = (target_actor(data.next_observations) + clipped_noise.to(device)).clamp(
                     envs.single_action_space.low[0], envs.single_action_space.high[0]