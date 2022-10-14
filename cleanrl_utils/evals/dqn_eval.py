4436ce45d1bc42206377a98cba6eabed98b1d913
Costa Huang <costa.huang@outlook.com>
2022-10-14 10:52:59 -0400

    pre-commit

--- cleanrl_utils/evals/dqn_eval.py
+++ cleanrl_utils/evals/dqn_eval.py
@@ -39,8 +39,10 @@ def evaluate(
 
     return episodic_returns
 
+
 if __name__ == "__main__":
     from huggingface_hub import hf_hub_download
+
     from cleanrl.dqn import QNetwork, make_env
 
     model_path = hf_hub_download(repo_id="cleanrl/CartPole-v1-dqn-seed1", filename="q_network.pth")
