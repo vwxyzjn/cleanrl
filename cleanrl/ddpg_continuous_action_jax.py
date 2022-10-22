4605546cacbf37037f6aa5e6346fa69beacd2a81
João G.M. Araújo <joaoguilhermearujo@gmail.com>
2022-10-22 11:18:28 +0000

    run pre-commit

--- cleanrl/ddpg_continuous_action_jax.py
+++ cleanrl/ddpg_continuous_action_jax.py
@@ -228,9 +228,9 @@ if __name__ == "__main__":
             actions = actor.apply(actor_state.params, obs)
             actions = np.array(
                 [
-                    (
-                        jax.device_get(actions)[0] + np.random.normal(0, action_scale * args.exploration_noise)[0]
-                    ).clip(envs.single_action_space.low, envs.single_action_space.high)
+                    (jax.device_get(actions)[0] + np.random.normal(0, action_scale * args.exploration_noise)[0]).clip(
+                        envs.single_action_space.low, envs.single_action_space.high
+                    )
                 ]
             )
 
