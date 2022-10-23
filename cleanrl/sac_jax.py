9c8e64228ce75230679f89becaa75f6879f438e9
Antonin Raffin <antonin.raffin@ensta.org>
2022-10-23 12:26:25 +0200

    Add SAC Jax version

---
+++ cleanrl/sac_jax.py
@@ -0,0 +1,489 @@
+# docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/sac/#sac_continuous_actionpy
+# Implementation adapted from https://github.com/araffin/sbx
+import argparse
+import os
+import random
+import time
+from distutils.util import strtobool
+from functools import partial
+from typing import Any, NamedTuple, Optional, Sequence, Union
+
+import flax
+import flax.linen as nn
+import gym
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+import pybullet_envs  # noqa
+import tensorflow_probability
+from flax.training.train_state import TrainState
+from stable_baselines3.common.buffers import ReplayBuffer
+from stable_baselines3.common.vec_env import DummyVecEnv
+from torch.utils.tensorboard import SummaryWriter
+
+tfp = tensorflow_probability.substrates.jax
+tfd = tfp.distributions
+
+
+class RLTrainState(TrainState):
+    target_params: flax.core.FrozenDict
+
+
+class ReplayBufferSamplesNp(NamedTuple):
+    observations: np.ndarray
+    actions: np.ndarray
+    next_observations: np.ndarray
+    dones: np.ndarray
+    rewards: np.ndarray
+
+
+class TanhTransformedDistribution(tfd.TransformedDistribution):
+    """
+    From https://github.com/ikostrikov/walk_in_the_park
+    otherwise mode is not defined for Squashed Gaussian
+    """
+
+    def __init__(self, distribution: tfd.Distribution, validate_args: bool = False):
+        super().__init__(distribution=distribution, bijector=tfp.bijectors.Tanh(), validate_args=validate_args)
+
+    def mode(self) -> jnp.ndarray:
+        return self.bijector.forward(self.distribution.mode())
+
+    @classmethod
+    def _parameter_properties(cls, dtype: Optional[Any], num_classes=None):
+        td_properties = super()._parameter_properties(dtype, num_classes=num_classes)
+        del td_properties["bijector"]
+        return td_properties
+
+
+class Critic(nn.Module):
+    n_units: int = 256
+
+    @nn.compact
+    def __call__(self, x: jnp.ndarray, action: jnp.ndarray) -> jnp.ndarray:
+        x = jnp.concatenate([x, action], -1)
+        x = nn.Dense(self.n_units)(x)
+        x = nn.relu(x)
+        x = nn.Dense(self.n_units)(x)
+        x = nn.relu(x)
+        x = nn.Dense(1)(x)
+        return x
+
+
+class VectorCritic(nn.Module):
+    n_units: int = 256
+    n_critics: int = 2
+
+    @nn.compact
+    def __call__(self, obs: jnp.ndarray, action: jnp.ndarray):
+        # Idea taken from https://github.com/perrin-isir/xpag
+        # Similar to https://github.com/tinkoff-ai/CORL for PyTorch
+        vmap_critic = nn.vmap(
+            Critic,
+            variable_axes={"params": 0},  # parameters not shared between the critics
+            split_rngs={"params": True},  # different initializations
+            in_axes=None,
+            out_axes=0,
+            axis_size=self.n_critics,
+        )
+        q_values = vmap_critic(
+            n_units=self.n_units,
+        )(obs, action)
+        return q_values
+
+
+class Actor(nn.Module):
+    action_dim: Sequence[int]
+    n_units: int = 256
+    log_std_min: float = -20
+    log_std_max: float = 2
+
+    def get_std(self):
+        # Make it work with gSDE
+        return jnp.array(0.0)
+
+    @nn.compact
+    def __call__(self, x: jnp.ndarray) -> tfd.Distribution:
+        x = nn.Dense(self.n_units)(x)
+        x = nn.relu(x)
+        x = nn.Dense(self.n_units)(x)
+        x = nn.relu(x)
+        mean = nn.Dense(self.action_dim)(x)
+        log_std = nn.Dense(self.action_dim)(x)
+        log_std = jnp.clip(log_std, self.log_std_min, self.log_std_max)
+        dist = TanhTransformedDistribution(
+            tfd.MultivariateNormalDiag(loc=mean, scale_diag=jnp.exp(log_std)),
+        )
+        return dist
+
+
+@partial(jax.jit, static_argnames="actor")
+def sample_action(actor, actor_state, obervations, key):
+    dist = actor.apply(actor_state.params, obervations)
+    action = dist.sample(seed=key)
+    return action
+
+
+@partial(jax.jit, static_argnames="actor")
+def select_action(actor, actor_state, obervations):
+    return actor.apply(actor_state.params, obervations).mode()
+
+
+def scale_action(action_space: gym.spaces.Box, action: np.ndarray) -> np.ndarray:
+    """
+    Rescale the action from [low, high] to [-1, 1]
+    (no need for symmetric action space)
+
+    :param action: Action to scale
+    :return: Scaled action
+    """
+    low, high = action_space.low, action_space.high
+    return 2.0 * ((action - low) / (high - low)) - 1.0
+
+
+def unscale_action(action_space: gym.spaces.Box, scaled_action: np.ndarray) -> np.ndarray:
+    """
+    Rescale the action from [-1, 1] to [low, high]
+    (no need for symmetric action space)
+
+    :param scaled_action: Action to un-scale
+    """
+    low, high = action_space.low, action_space.high
+    return low + (0.5 * (scaled_action + 1.0) * (high - low))
+
+
+class EntropyCoef(nn.Module):
+    ent_coef_init: float = 1.0
+
+    @nn.compact
+    def __call__(self) -> jnp.ndarray:
+        log_ent_coef = self.param("log_ent_coef", init_fn=lambda key: jnp.full((), jnp.log(self.ent_coef_init)))
+        return jnp.exp(log_ent_coef)
+
+
+@partial(jax.jit, static_argnames=["tau"])
+def soft_update(tau: float, qf_state: RLTrainState) -> RLTrainState:
+    qf_state = qf_state.replace(target_params=optax.incremental_update(qf_state.params, qf_state.target_params, tau))
+    return qf_state
+
+
+def parse_args():
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
+        help="the name of this experiment")
+    parser.add_argument("--seed", type=int, default=1,
+        help="seed of the experiment")
+    parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="if toggled, this experiment will be tracked with Weights and Biases")
+    parser.add_argument("--wandb-project-name", type=str, default="cleanRL",
+        help="the wandb's project name")
+    parser.add_argument("--wandb-entity", type=str, default=None,
+        help="the entity (team) of wandb's project")
+    parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="whether to capture videos of the agent performances (check out `videos` folder)")
+
+    # Algorithm specific arguments
+    parser.add_argument("--env-id", type=str, default="HopperBulletEnv-v0",
+        help="the id of the environment")
+    parser.add_argument("--total-timesteps", type=int, default=1000000,
+        help="total timesteps of the experiments")
+    parser.add_argument("--buffer-size", type=int, default=int(1e6),
+        help="the replay memory buffer size")
+    parser.add_argument("--gamma", type=float, default=0.99,
+        help="the discount factor gamma")
+    parser.add_argument("--tau", type=float, default=0.005,
+        help="target smoothing coefficient (default: 0.005)")
+    parser.add_argument("--batch-size", type=int, default=256,
+        help="the batch size of sample from the reply memory")
+    parser.add_argument("--learning-starts", type=int, default=5e3,
+        help="timestep to start learning")
+    parser.add_argument("--policy-lr", type=float, default=3e-4,
+        help="the learning rate of the policy network optimizer")
+    parser.add_argument("--q-lr", type=float, default=1e-3,
+        help="the learning rate of the Q network network optimizer")
+    parser.add_argument("--n-critics", type=int, default=2,
+        help="the number of critic networks")
+    parser.add_argument("--policy-frequency", type=int, default=1,
+        help="the frequency of training policy (delayed)")
+    parser.add_argument("--target-network-frequency", type=int, default=1, # Denis Yarats' implementation delays this by 2.
+        help="the frequency of updates for the target nerworks")
+    parser.add_argument("--alpha", type=float, default=0.2,
+            help="Entropy regularization coefficient.")
+    parser.add_argument("--autotune", type=lambda x:bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="automatic tuning of the entropy coefficient")
+    args = parser.parse_args()
+    # fmt: on
+    return args
+
+
+def make_env(env_id, seed, idx, capture_video, run_name):
+    def thunk():
+        env = gym.make(env_id)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        if capture_video:
+            if idx == 0:
+                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        env.seed(seed)
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+        return env
+
+    return thunk
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        import wandb
+
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    key = jax.random.PRNGKey(args.seed)
+
+    # env setup
+    envs = DummyVecEnv([make_env(args.env_id, args.seed, 0, args.capture_video, run_name)])
+    assert isinstance(envs.action_space, gym.spaces.Box), "only continuous action space is supported"
+
+    # Create networks
+    key, actor_key, qf_key, ent_key = jax.random.split(key, 4)
+
+    obs = jnp.array([envs.observation_space.sample()])
+    action = jnp.array([envs.action_space.sample()])
+
+    actor = Actor(action_dim=np.prod(envs.action_space.shape))
+
+    actor_state = TrainState.create(
+        apply_fn=actor.apply,
+        params=actor.init(actor_key, obs),
+        tx=optax.adam(learning_rate=args.policy_lr),
+    )
+
+    qf = VectorCritic(n_critics=args.n_critics)
+
+    qf_state = RLTrainState.create(
+        apply_fn=qf.apply,
+        params=qf.init({"params": qf_key}, obs, action),
+        target_params=qf.init({"params": qf_key}, obs, action),
+        tx=optax.adam(learning_rate=args.q_lr),
+    )
+
+    actor.apply = jax.jit(actor.apply)
+    qf.apply = jax.jit(qf.apply)
+
+    # Automatic entropy tuning
+    if args.autotune:
+        ent_coef = EntropyCoef(ent_coef_init=1.0)
+        target_entropy = -np.prod(envs.action_space.shape).astype(np.float32)
+        ent_coef_state = TrainState.create(
+            apply_fn=ent_coef.apply,
+            params=ent_coef.init(ent_key)["params"],
+            tx=optax.adam(learning_rate=args.q_lr),
+        )
+
+    else:
+        raise NotImplementedError()
+        # ent_coef = args.alpha
+
+    # Define update functions here to limit the need for static argname
+    @jax.jit
+    def update_critic(
+        actor_state: TrainState,
+        qf_state: RLTrainState,
+        ent_coef_state: TrainState,
+        observations: np.ndarray,
+        actions: np.ndarray,
+        next_observations: np.ndarray,
+        rewards: np.ndarray,
+        dones: np.ndarray,
+        key,
+    ):
+        key, noise_key = jax.random.split(key, 2)
+        # sample action from the actor
+        dist = actor.apply(actor_state.params, next_observations)
+        next_state_actions = dist.sample(seed=noise_key)
+        next_log_prob = dist.log_prob(next_state_actions)
+
+        ent_coef_value = ent_coef.apply({"params": ent_coef_state.params})
+
+        qf_next_values = qf.apply(qf_state.target_params, next_observations, next_state_actions)
+
+        next_q_values = jnp.min(qf_next_values, axis=0)
+        # td error + entropy term
+        next_q_values = next_q_values - ent_coef_value * next_log_prob.reshape(-1, 1)
+        # shape is (batch_size, 1)
+        target_q_values = rewards.reshape(-1, 1) + (1 - dones.reshape(-1, 1)) * args.gamma * next_q_values
+
+        def mse_loss(params):
+            # shape is (n_critics, batch_size, 1)
+            current_q_values = qf.apply(params, observations, actions)
+            return ((target_q_values - current_q_values) ** 2).mean()
+
+        qf_loss_value, grads = jax.value_and_grad(mse_loss, has_aux=False)(qf_state.params)
+        qf_state = qf_state.apply_gradients(grads=grads)
+
+        return (
+            qf_state,
+            (qf_loss_value, ent_coef_value),
+            key,
+        )
+
+    @jax.jit
+    def update_actor(
+        actor_state: RLTrainState,
+        qf_state: RLTrainState,
+        ent_coef_state: TrainState,
+        observations: np.ndarray,
+        key,
+    ):
+        key, noise_key = jax.random.split(key, 2)
+
+        def actor_loss(params):
+
+            dist = actor.apply(params, observations)
+            actor_actions = dist.sample(seed=noise_key)
+            log_prob = dist.log_prob(actor_actions).reshape(-1, 1)
+
+            qf_pi = qf.apply(qf_state.params, observations, actor_actions)
+            # Take min among all critics
+            min_qf_pi = jnp.min(qf_pi, axis=0)
+            ent_coef_value = ent_coef.apply({"params": ent_coef_state.params})
+            actor_loss = (ent_coef_value * log_prob - min_qf_pi).mean()
+            return actor_loss, -log_prob.mean()
+
+        (actor_loss_value, entropy), grads = jax.value_and_grad(actor_loss, has_aux=True)(actor_state.params)
+        actor_state = actor_state.apply_gradients(grads=grads)
+
+        return actor_state, qf_state, actor_loss_value, key, entropy
+
+    @jax.jit
+    def update_temperature(ent_coef_state: TrainState, entropy: float):
+        def temperature_loss(temp_params):
+            ent_coef_value = ent_coef.apply({"params": temp_params})
+            ent_coef_loss = ent_coef_value * (entropy - target_entropy).mean()
+            return ent_coef_loss
+
+        ent_coef_loss, grads = jax.value_and_grad(temperature_loss)(ent_coef_state.params)
+        ent_coef_state = ent_coef_state.apply_gradients(grads=grads)
+
+        return ent_coef_state, ent_coef_loss
+
+    envs.observation_space.dtype = np.float32
+    rb = ReplayBuffer(
+        args.buffer_size,
+        envs.observation_space,
+        envs.action_space,
+        device="cpu",  # force cpu device to easy torch -> numpy conversion
+        handle_timeout_termination=True,
+    )
+    start_time = time.time()
+
+    # TRY NOT TO MODIFY: start the game
+    obs = envs.reset()
+    for global_step in range(args.total_timesteps):
+        # ALGO LOGIC: put action logic here
+        if global_step < args.learning_starts:
+            actions = np.array([envs.action_space.sample() for _ in range(envs.num_envs)])
+        else:
+            key, noise_key = jax.random.split(key, 2)
+            actions = np.array(sample_action(actor, actor_state, obs, noise_key))
+            # Clip due to numerical instability
+            actions = np.clip(actions, -1, 1)
+            # Rescale to proper domain when using squashing
+            actions = unscale_action(envs.action_space, actions)
+
+        # TRY NOT TO MODIFY: execute the game and log data.
+        next_obs, rewards, dones, infos = envs.step(actions)
+
+        # TRY NOT TO MODIFY: record rewards for plotting purposes
+        for info in infos:
+            if "episode" in info.keys():
+                print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
+                writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+                writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
+                break
+
+        # TRY NOT TO MODIFY: save data to reply buffer; handle `terminal_observation`
+        real_next_obs = next_obs.copy()
+        for idx, d in enumerate(dones):
+            if d:
+                real_next_obs[idx] = infos[idx]["terminal_observation"]
+
+        # Store the scaled action
+        scaled_actions = scale_action(envs.action_space, actions)
+        rb.add(obs, real_next_obs, actions, rewards, dones, infos)
+
+        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
+        obs = next_obs
+
+        # ALGO LOGIC: training.
+        if global_step > args.learning_starts:
+            data = rb.sample(args.batch_size)
+            # Convert to numpy
+            data = ReplayBufferSamplesNp(
+                data.observations.numpy(),
+                data.actions.numpy(),
+                data.next_observations.numpy(),
+                data.dones.numpy().flatten(),
+                data.rewards.numpy().flatten(),
+            )
+
+            qf_state, (qf_loss_value, ent_coef_value), key = update_critic(
+                actor_state,
+                qf_state,
+                ent_coef_state,
+                data.observations,
+                data.actions,
+                data.next_observations,
+                data.rewards,
+                data.dones,
+                key,
+            )
+
+            if global_step % args.policy_frequency == 0:  # TD 3 Delayed update support
+                (actor_state, qf_state, actor_loss_value, key, entropy) = update_actor(
+                    actor_state,
+                    qf_state,
+                    ent_coef_state,
+                    data.observations,
+                    key,
+                )
+
+                if args.autotune:
+                    ent_coef_state, ent_coef_loss = update_temperature(ent_coef_state, entropy)
+
+            # update the target networks
+            if global_step % args.target_network_frequency == 0:
+                qf_state = soft_update(args.tau, qf_state)
+
+            if global_step % 100 == 0:
+                # writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
+                # writer.add_scalar("losses/qf2_values", qf2_a_values.mean().item(), global_step)
+                writer.add_scalar("losses/qf_loss", qf_loss_value.item(), global_step)
+                writer.add_scalar("losses/actor_loss", actor_loss_value.item(), global_step)
+                writer.add_scalar("losses/alpha", ent_coef_value.item(), global_step)
+                print("SPS:", int(global_step / (time.time() - start_time)))
+                writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+                if args.autotune:
+                    writer.add_scalar("losses/alpha_loss", ent_coef_loss.item(), global_step)
+
+    envs.close()
+    writer.close()
