39a30c4d7765e185fa6370f463f82d6721c8b6c4
David Slayback <slayback.d@northeastern.edu>
2022-10-28 15:07:35 -0400

    Benchmark scripts

---
+++ benchmark/ppo_speed.sh
@@ -0,0 +1,32 @@
+# export WANDB_ENTITY=openrlbenchmark
+
+poetry install
+OMP_NUM_THREADS=1 xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
+    --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 \
+    --command "poetry run python cleanrl/ppo.py --cuda False --track --capture-video" \
+    --num-seeds 3 \
+    --workers 9
+
+OMP_NUM_THREADS=1 xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
+    --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 \
+    --command "poetry run python cleanrl/ppo_faster_1.py --cuda False --track --capture-video" \
+    --num-seeds 3 \
+    --workers 9
+
+OMP_NUM_THREADS=1 xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
+    --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 \
+    --command "poetry run python cleanrl/ppo_faster_2.py --cuda False --track --capture-video" \
+    --num-seeds 3 \
+    --workers 9
+
+OMP_NUM_THREADS=1 xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
+    --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 \
+    --command "poetry run python cleanrl/ppo_faster_3.py --cuda False --track --capture-video" \
+    --num-seeds 3 \
+    --workers 9
+
+OMP_NUM_THREADS=1 xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
+    --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 \
+    --command "poetry run python cleanrl/ppo_faster_4.py --cuda False --track --capture-video" \
+    --num-seeds 3 \
+    --workers 9
---
+++ benchmark/rnd_speed.sh
@@ -0,0 +1,14 @@
+# export WANDB_ENTITY=openrlbenchmark
+
+poetry install --with envpool
+xvfb-run -a python -m cleanrl_utils.benchmark \
+    --env-ids MontezumaRevenge-v5 \
+    --command "poetry run python cleanrl/ppo_rnd_envpool.py --track" \
+    --num-seeds 1 \
+    --workers 1
+
+xvfb-run -a python -m cleanrl_utils.benchmark \
+    --env-ids MontezumaRevenge-v5 \
+    --command "poetry run python cleanrl/ppo_rnd_envpool_faster_3.py --track" \
+    --num-seeds 1 \
+    --workers 1
