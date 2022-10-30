f9b303e64122c412f4ff2f2ce8666efed8aafa1b
David Slayback <slayback.d@northeastern.edu>
2022-10-30 14:08:00 -0400

    Update rnd_speed.sh

--- benchmark/rnd_speed.sh
+++ benchmark/rnd_speed.sh
@@ -3,12 +3,12 @@
 poetry install --with envpool
 xvfb-run -a python -m cleanrl_utils.benchmark \
     --env-ids MontezumaRevenge-v5 \
-    --command "poetry run python cleanrl/ppo_rnd_envpool.py --track" \
-    --num-seeds 1 \
-    --workers 1
+    --command "poetry run python cleanrl/ppo_rnd_envpool.py --track --num-iterations-obs-norm-init 2 --total-timesteps 20000000" \
+    --num-seeds 2 \
+    --workers 2
 
 xvfb-run -a python -m cleanrl_utils.benchmark \
     --env-ids MontezumaRevenge-v5 \
-    --command "poetry run python cleanrl/ppo_rnd_envpool_faster_3.py --track" \
-    --num-seeds 1 \
-    --workers 1
+    --command "poetry run python cleanrl/ppo_rnd_envpool_faster_3.py --track --num-iterations-obs-norm-init 2 --total-timesteps 20000000" \
+    --num-seeds 2 \
+    --workers 2
