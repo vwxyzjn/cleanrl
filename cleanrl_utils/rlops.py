1353ecbe6381df213eff76ebb425f285167e0dcb
Costa Huang <costa.huang@outlook.com>
2022-11-08 18:40:19 -0500

    support more advanced filters

--- cleanrl_utils/rlops.py
+++ cleanrl_utils/rlops.py
@@ -11,6 +11,8 @@ import wandb
 import wandb.apis.reports as wb  # noqa
 from expt import Hypothesis, Run
 from rich.console import Console
+from urllib.parse import urlparse, parse_qs
+import seaborn as sns
 
 wandb.require("report-editing")
 api = wandb.Api()
@@ -19,13 +21,12 @@ api = wandb.Api()
 def parse_args():
     # fmt: off
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exp-name", type=str, default="ddpg_continuous_action_jax",
-        help="the name of this experiment")
     parser.add_argument("--wandb-project-name", type=str, default="cleanrl",
         help="the wandb's project name")
     parser.add_argument("--wandb-entity", type=str, default="openrlbenchmark",
         help="the entity (team) of wandb's project")
-    parser.add_argument("--tags", nargs="+", default=["v1.0.0b2-9-g4605546", "rlops-pilot"],
+    # TODO: update the docs for filter
+    parser.add_argument("--filters", nargs="+", default=["v1.0.0b2-9-g4605546", "rlops-pilot"],
         help='the tags of the runsets (e.g., `--tags v1.0.0b2-9-g4605546 rlops-pilot` and you can also use `--tags "v1.0.0b2-9-g4605546;latest"` to filter runs with multiple tags)')
     parser.add_argument("--env-ids", nargs="+", default=["Hopper-v2", "Walker2d-v2", "HalfCheetah-v2"],
         help="the ids of the environment to compare")
@@ -55,14 +56,14 @@ def create_hypothesis(name: str, wandb_runs: List[wandb.apis.public.Run], scan_h
         runs += [Run(f"seed{idx}", wandb_run)]
     return Hypothesis(name, runs)
 
-
 class Runset:
-    def __init__(self, name: str, filters: dict, entity: str, project: str, groupby: str = ""):
+    def __init__(self, name: str, filters: dict, entity: str, project: str, groupby: str = "", color: str = "#000000"):
         self.name = name
         self.filters = filters
         self.entity = entity
         self.project = project
         self.groupby = groupby
+        self.color = color
 
     @property
     def runs(self):
@@ -70,7 +71,7 @@ class Runset:
 
     @property
     def report_runset(self):
-        return wb.RunSet(
+        return wb.Runset(
             name=self.name,
             entity=self.entity,
             project=self.project,
@@ -90,37 +91,44 @@ def compare(
 ):
     blocks = []
     for idx, env_id in enumerate(env_ids):
+        pg = wb.PanelGrid(
+            runsets=[runsets[idx].report_runset for runsets in runsetss],
+            panels=[
+                wb.LinePlot(
+                    x="global_step",
+                    y=["charts/episodic_return"],
+                    title=env_id,
+                    title_x="Steps",
+                    title_y="Episodic Return",
+                    max_runs_to_show=100,
+                    smoothing_factor=0.8,
+                    groupby_rangefunc="stderr",
+                    legend_template="${runsetName}",
+                ),
+                wb.LinePlot(
+                    x="_runtime",
+                    y=["charts/episodic_return"],
+                    title=env_id,
+                    title_y="Episodic Return",
+                    max_runs_to_show=100,
+                    smoothing_factor=0.8,
+                    groupby_rangefunc="stderr",
+                    legend_template="${runsetName}",
+                ),
+                # wb.MediaBrowser(
+                #     num_columns=2,
+                #     media_keys="videos",
+                # ),
+            ],
+        )
+        custom_run_colors = {}
+        for runsets in runsetss:
+            custom_run_colors.update({
+                (runsets[idx].report_runset.name, runsets[idx].runs[0].config["exp_name"]): runsets[idx].color
+            })
+        pg.custom_run_colors = custom_run_colors # IMPORTANT: custom_run_colors is implemented as a custom `setter` that needs to be overwritten unlike regular dictionaries
         blocks += [
-            wb.PanelGrid(
-                runsets=[runsets[idx].report_runset for runsets in runsetss],
-                panels=[
-                    wb.LinePlot(
-                        x="global_step",
-                        y=["charts/episodic_return"],
-                        title=env_id,
-                        title_x="Steps",
-                        title_y="Episodic Return",
-                        max_runs_to_show=100,
-                        smoothing_factor=0.8,
-                        groupby_rangefunc="stderr",
-                        legend_template="${runsetName}",
-                    ),
-                    wb.LinePlot(
-                        x="_runtime",
-                        y=["charts/episodic_return"],
-                        title=env_id,
-                        title_y="Episodic Return",
-                        max_runs_to_show=100,
-                        smoothing_factor=0.8,
-                        groupby_rangefunc="stderr",
-                        legend_template="${runsetName}",
-                    ),
-                    # wb.MediaBrowser(
-                    #     num_columns=2,
-                    #     media_keys="videos",
-                    # ),
-                ],
-            ),
+            pg
         ]
 
     nrows = np.ceil(len(env_ids) / ncols).astype(int)
@@ -160,6 +168,7 @@ def compare(
             err_style="band",
             std_alpha=0.1,
             rolling=rolling,
+            colors=[runsets[idx].color for runsets in runsetss],
             # n_samples=500,
             legend=False,
         )
@@ -167,47 +176,20 @@ def compare(
     print(result_table)
 
     h, l = ax.get_legend_handles_labels()
-    fig.legend(h, l, loc="upper center", ncol=2)
-    fig.subplots_adjust(top=0.9)
+    fig.legend(h, l, loc="upper center", ncol=ncols)
+    num_legend_rows = len(h) // 2
+    # dynamically adjust the top of subplot to make room for legend
+    fig.subplots_adjust(top=1 - 0.07 * num_legend_rows)
     # remove the empty axes
-    for ax in axes.flatten()[len(env_ids) :]:
+    for ax in axes.flatten()[len(env_ids):]:
         ax.remove()
 
     print(f"saving figure to {output_filename}")
-    os.makedirs(os.path.dirname(output_filename), exist_ok=True)
+    if os.path.dirname(output_filename) != "":
+        os.makedirs(os.path.dirname(output_filename), exist_ok=True)
     plt.savefig(f"{output_filename}", bbox_inches="tight")
     plt.savefig(f"{output_filename.replace('.png', '.pdf')}", bbox_inches="tight")
     return blocks
-
-
-def parse_str(tag_str: str) -> List[List[Dict]]:
-    """
-    Extract the user, include tags, and exclude tags from the tag string `tag_str`.
-
-    `tag_str` has the syntax "tag1;tag2!tag3;tag4?user1", where the tag1 and tag2 are the
-    include tags, tag3 and tag4 are the exclude tags, and user1 is the user
-
-    :param tag_str: a string of the form
-    :return: a list of filters for wandb
-    """
-    user = []
-    if "?" in tag_str:
-        user = tag_str.split("?")[1]
-        tag_str = tag_str.split("?")[0]
-    # extract exclude tags
-    exclude_tag_groups = []
-    if "!" in tag_str:
-        exclude_tag_groups = tag_str.split("!")[1].split(";")
-        tag_str = tag_str.split("!")[0]
-    # extract include tags
-    include_tag_groups = tag_str.split(";")
-
-    # convert to wandb filters
-    include_tag_groups = [{"tags": {"$in": [tag]}} for tag in include_tag_groups]
-    exclude_tag_groups = [{"tags": {"$nin": [tag]}} for tag in exclude_tag_groups]
-    user = [{"username": user}] if len(user) > 0 else []
-
-    return include_tag_groups, exclude_tag_groups, user
 
 
 if __name__ == "__main__":
@@ -215,33 +197,39 @@ if __name__ == "__main__":
     console = Console()
     blocks = []
     runsetss = []
-    for tag_str in args.tags:
-        include_tag_groups, exclude_tag_groups, user = parse_str(tag_str)
+    colors = sns.color_palette(n_colors=len(args.filters))
+    for filter_str, color in zip(args.filters, colors):
+        print("=========", filter_str)
+        parse_result = urlparse(filter_str)
+        exp_name = parse_result.path
+        query = parse_qs(parse_result.query)
+        user = [{"username": query["user"][0]}] if "user" in query else []
+        include_tag_groups = [{"tags": {"$in": [tag]}} for tag in query["tag"]] if "tag" in query else []
         runsets = []
         for env_id in args.env_ids:
             runsets += [
                 Runset(
-                    name=f"CleanRL's {args.exp_name} ({tag_str})",
+                    name=f"CleanRL's {exp_name} ({query})",
                     filters={
                         "$and": [
                             {"config.env_id.value": env_id},
                             *include_tag_groups,
-                            *exclude_tag_groups,
                             *user,
-                            {"config.exp_name.value": args.exp_name},
+                            {"config.exp_name.value": exp_name},
                         ]
                     },
                     entity=args.wandb_entity,
                     project=args.wandb_project_name,
                     groupby="exp_name",
+                    color=color,
                 )
             ]
             console.print(
-                f"CleanRL's {args.exp_name} [green]({tag_str})[/] in [purple]{env_id}[/] has {len(runsets[-1].runs)} runs"
+                f"CleanRL's {exp_name} [green]({query})[/] in [purple]{env_id}[/] has {len(runsets[-1].runs)} runs"
             )
             for run in runsets[-1].runs:
                 console.print(f"┣━━ [link={run.url}]{run.name}[/link] with tags = {run.tags}")
-            assert len(runsets[0].runs) > 0, f"CleanRL's {args.exp_name} ({tag_str}) in {env_id} has no runs"
+            assert len(runsets[0].runs) > 0, f"CleanRL's {exp_name} ({query}) in {env_id} has no runs"
         runsetss += [runsets]
 
     blocks = compare(
@@ -257,7 +245,8 @@ if __name__ == "__main__":
         print("saving report")
         report = wb.Report(
             project="cleanrl",
-            title=f"Regression Report: {args.exp_name} ({args.tags})",
+            title=f"Regression Report: {exp_name}",
+            description=str(args.filters),
             blocks=blocks,
         )
         report.save()
