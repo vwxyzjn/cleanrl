f4501be27e49aac99ec533072d14dec751ebe37d
Costa Huang <costa.huang@outlook.com>
2022-11-19 20:41:09 -0500

    remove `OMP_NUM_THREADS=1`

--- benchmark/ppo_dna.sh
+++ benchmark/ppo_dna.sh
@@ -2,7 +2,7 @@
 
 # comparison with PPO-DNA paper results on "Atari-5" envs
 poetry install -E envpool
-OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
+xvfb-run -a python -m cleanrl_utils.benchmark \
     --env-ids BattleZone-v5 DoubleDunk-v5 NameThisGame-v5 Phoenix-v5 Qbert-v5 \
     --command "poetry run python cleanrl/ppo_dna_atari_envpool.py --anneal-lr False --total-timesteps 50000000 --track" \
     --num-seeds 3 \
@@ -10,8 +10,8 @@ OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
 
 # comparison with CleanRL ppo_atari_envpool.py
 poetry install -E envpool
-OMP_NUM_THREADS=1 xvfb-run -a python -m cleanrl_utils.benchmark \
-    --env-ids Pong-v5 BeamRider-v5 Breakout-v5 Tennis-v5 \
+xvfb-run -a python -m cleanrl_utils.benchmark \
+    --env-ids Pong-v5 BeamRider-v5 Breakout-v5 \
     --command "poetry run python cleanrl/ppo_dna_atari_envpool.py --track" \
     --num-seeds 3 \
     --workers 1
