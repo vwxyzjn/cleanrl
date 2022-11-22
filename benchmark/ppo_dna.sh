52c9c551c7a08baffccd71bb4f396f859cc0f8c1
Costa Huang <costa.huang@outlook.com>
2022-11-22 09:46:10 -0500

    update script

--- benchmark/ppo_dna.sh
+++ benchmark/ppo_dna.sh
@@ -2,8 +2,16 @@
 
 # comparison with PPO-DNA paper results on "Atari-5" envs
 poetry install -E envpool
-xvfb-run -a python -m cleanrl_utils.benchmark \
+poetry run python -m cleanrl_utils.benchmark \
     --env-ids BattleZone-v5 DoubleDunk-v5 NameThisGame-v5 Phoenix-v5 Qbert-v5 \
-    --command "poetry run python cleanrl/ppo_dna_atari_envpool.py --anneal-lr False --track" \
+    --command "poetry run python cleanrl/ppo_dna_atari_envpool.py --anneal-lr False --total-timesteps 50000000 --track" \
+    --num-seeds 3 \
+    --workers 1
+
+# comparison with CleanRL ppo_atari_envpool.py
+poetry install -E envpool
+poetry run python -m cleanrl_utils.benchmark \
+    --env-ids Pong-v5 BeamRider-v5 Breakout-v5 Tennis-v5 \
+    --command "poetry run python cleanrl/ppo_dna_atari_envpool.py --track" \
     --num-seeds 3 \
     --workers 1
