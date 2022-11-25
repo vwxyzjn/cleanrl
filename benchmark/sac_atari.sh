e199e39bc657dcf634079f0efaca0299bca7e508
Timo Klein <tkleia34@gmail.com>
2022-11-25 10:26:22 +0100

    fix benchmark installation

--- benchmark/sac_atari.sh
+++ benchmark/sac_atari.sh
@@ -1,4 +1,4 @@
-poetry install -E atari
+poetry install --with atari
 OMP_NUM_THREADS=1 python -m cleanrl_utils.benchmark \
     --env-ids PongNoFrameskip-v4 BreakoutNoFrameskip-v4 BeamRiderNoFrameskip-v4 \
     --command "poetry run python cleanrl/sac_atari.py --cuda True --track" \
