2619922293ddb3768c48dba091c3e3903d045509
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2022-11-29 12:53:07 +0900

    Queued up runs with B 250 and T 10: shorter sequence could help make training faster ?

--- cleanrl/dreamer_runs.sh
+++ cleanrl/dreamer_runs.sh
@@ -5,61 +5,81 @@ export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
 ###############################################
 # region: Atari Pong                          #
 # Dreamer with discrete latents
-export CUDA_VISIBLE_DEVICES=7
-(sleep 1s && python dreamer_atari.py \
-    --track --capture-video \
-    --env-id "BreakoutNoFrameskip-v4" \
-    --exp-name "dreamer" \
-    --seed 1 \
-) >& /dev/null &
+# export CUDA_VISIBLE_DEVICES=7
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer" \
+#     --seed 1 \
+# ) >& /dev/null &
 
-export CUDA_VISIBLE_DEVICES=6
-(sleep 1s && python dreamer_atari.py \
-    --track --capture-video \
-    --env-id "BreakoutNoFrameskip-v4" \
-    --exp-name "dreamer" \
-    --seed 2 \
-) >& /dev/null &
+# export CUDA_VISIBLE_DEVICES=6
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer" \
+#     --seed 2 \
+# ) >& /dev/null &
+
+
+# Dreamer with discrete latents, bugger batch and shorter sequences
+# export CUDA_VISIBLE_DEVICES=1
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer__B_250_T_10" \
+#     --batch-size 250 --batch-length 10 \
+#     --seed 1 \
+# ) >& /dev/null &
+
+# export CUDA_VISIBLE_DEVICES=0
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer__B_250_T_10" \
+#     --batch-size 250 --batch-length 10 \
+#     --seed 2 \
+# ) >& /dev/null &
 
 
 # Dreamer with discrete latents
-export CUDA_VISIBLE_DEVICES=5
-(sleep 1s && python dreamer_atari.py \
-    --track --capture-video \
-    --env-id "BreakoutNoFrameskip-v4" \
-    --exp-name "dreamer__numenvs_4" \
-    --num-envs 4 \
-    --seed 1 \
-) >& /dev/null &
+# export CUDA_VISIBLE_DEVICES=5
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer__numenvs_4" \
+#     --num-envs 4 \
+#     --seed 1 \
+# ) >& /dev/null &
 
-export CUDA_VISIBLE_DEVICES=4
-(sleep 1s && python dreamer_atari.py \
-    --track --capture-video \
-    --env-id "BreakoutNoFrameskip-v4" \
-    --exp-name "dreamer__numenvs_4" \
-    --num-envs 4 \
-    --seed 2 \
-) >& /dev/null &
+# export CUDA_VISIBLE_DEVICES=4
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer__numenvs_4" \
+#     --num-envs 4 \
+#     --seed 2 \
+# ) >& /dev/null &
 
 
 # Dreamer with continuous latents
-export CUDA_VISIBLE_DEVICES=3
-(sleep 1s && python dreamer_atari.py \
-    --track --capture-video \
-    --env-id "BreakoutNoFrameskip-v4" \
-    --exp-name "dreamer_cont_latents" \
-    --rssm-discrete 0 \
-    --seed 1 \
-) >& /dev/null &
+# export CUDA_VISIBLE_DEVICES=3
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer_cont_latents" \
+#     --rssm-discrete 0 \
+#     --seed 1 \
+# ) >& /dev/null &
 
-export CUDA_VISIBLE_DEVICES=2
-(sleep 1s && python dreamer_atari.py \
-    --track --capture-video \
-    --env-id "BreakoutNoFrameskip-v4" \
-    --exp-name "dreamer_cont_latents" \
-    --rssm-discrete 0 \
-    --seed 2 \
-) >& /dev/null &
+# export CUDA_VISIBLE_DEVICES=2
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer_cont_latents" \
+#     --rssm-discrete 0 \
+#     --seed 2 \
+# ) >& /dev/null &
 
 # endregion: Atari Pong                       #
 ###############################################
