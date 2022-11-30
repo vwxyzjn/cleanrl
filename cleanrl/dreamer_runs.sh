1371fe9e49f63ff18cc0d2f4d44618a22532b4bb
Fousslan F.J. Dossa <dosssman@hotmail.fr>
2022-11-30 14:00:20 +0900

    Added runs for dreamer_atari with more frequent training

--- cleanrl/dreamer_runs.sh
+++ cleanrl/dreamer_runs.sh
@@ -21,6 +21,24 @@ export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
 #     --seed 2 \
 # ) >& /dev/null &
 
+# Dreamer with discret latents, train more frequently
+# export CUDA_VISIBLE_DEVICES=3
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer__trainevery_4" \
+#     --train-every 4 \
+#     --seed 1 \
+# ) >& /dev/null &
+# export CUDA_VISIBLE_DEVICES=2
+# (sleep 1s && python dreamer_atari.py \
+#     --track --capture-video \
+#     --env-id "BreakoutNoFrameskip-v4" \
+#     --exp-name "dreamer__trainevery_4" \
+#     --train-every 4 \
+#     --seed 2 \
+# ) >& /dev/null &
+
 # Dreamer with discrete latents and AMP
 # export CUDA_VISIBLE_DEVICES=5
 # (sleep 1s && python dreamer_atari_amp.py \
