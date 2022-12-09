394106f07682f6c5bc5f46a9651dae089cdf613c
Rousslan F.J. Dossa (dosssman) <dosssman@hotmail.fr>
2022-12-09 10:23:13 +0900

    Added more POng runs with seeds for the baseline version

--- cleanrl/dreamer_runs.sh
+++ cleanrl/dreamer_runs.sh
@@ -102,19 +102,7 @@ export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
 ###############################################
 
 ###############################################
-# region: Atari Pong                           #
-# export CUDA_VISIBLE_DEVICES=6
-# for seed in 1 2; do
-# for seed in 3 4; do
-#     (sleep 1s && python dreamer_atari.py \
-#         --track --capture-video \
-#         --env-id "PongNoFrameskip-v4" \
-#         --batch-size 50 --batch-length 50 \
-#         --train-every 16 \
-#         --exp-name "dreamer_B_50_T_50_trnev_16" \
-#         --seed $seed \
-#     ) >& /dev/null &
-# done
+# region: Atari Pong                          #
 
 # export CUDA_VISIBLE_DEVICES=5
 # for seed in 1 2; do
@@ -129,8 +117,9 @@ export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
 # done
 
 # Dreamer Disc Baseline, B=50, T=50, train-every=4
-# export CUDA_VISIBLE_DEVICES=0
-# for seed in 1 2; do
+# export CUDA_VISIBLE_DEVICES=2
+# # for seed in 1 2; do
+# for seed in 2 3 4; do
 #     (sleep 1s && python dreamer_atari.py \
 #         --track --capture-video \
 #         --env-id "PongNoFrameskip-v4" \
