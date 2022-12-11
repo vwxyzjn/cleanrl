1e904a33a5c96bf5deb4c6fc039b78c0e13df41b
Costa Huang <costa.huang@outlook.com>
2022-12-10 19:27:38 -0500

    set default env = HalfCheetah-v4

--- cleanrl/gymnasium_support/ppo_continuous_action.py
+++ cleanrl/gymnasium_support/ppo_continuous_action.py
@@ -35,7 +35,7 @@ def parse_args():
         help="whether to capture videos of the agent performances (check out `videos` folder)")
 
     # Algorithm specific arguments
-    parser.add_argument("--env-id", type=str, default="HalfCheetahBulletEnv-v0",
+    parser.add_argument("--env-id", type=str, default="HalfCheetah-v4",
         help="the id of the environment")
     parser.add_argument("--total-timesteps", type=int, default=1000000,
         help="total timesteps of the experiments")
