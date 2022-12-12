55f22090c3c6a713fa99875dc90eeada94eb195d
Costa Huang <costa.huang@outlook.com>
2022-12-11 23:09:35 -0500

    Fix CI

--- tests/test_mujoco.py
+++ tests/test_mujoco.py
@@ -6,12 +6,12 @@ def test_mujoco():
     Test mujoco
     """
     subprocess.run(
-        "python cleanrl/ppo_continuous_action.py --env-id Hopper-v4 --num-envs 1 --num-steps 32 --total-timesteps 128",
+        "python cleanrl/ppo_continuous_action.py --env-id Hopper-v4 --num-envs 1 --num-steps 64 --total-timesteps 128",
         shell=True,
         check=True,
     )
     subprocess.run(
-        "python cleanrl/ppo_continuous_action.py --env-id dm_control/cartpole-balance-v0 --num-envs 1 --num-steps 32 --total-timesteps 128",
+        "python cleanrl/ppo_continuous_action.py --env-id dm_control/cartpole-balance-v0 --num-envs 1 --num-steps 64 --total-timesteps 128",
         shell=True,
         check=True,
     )
