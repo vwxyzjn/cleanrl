66c3c62a23c09d97375975810d3d4667bc30de83
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2022-12-22 16:43:10 +0900

    Fixed Breakout with shorter batch length

--- cleanrl/dreamer_runs.sh
+++ cleanrl/dreamer_runs.sh
@@ -32,12 +32,12 @@ export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
 
     # region: Investigating batch length trade off on walltime and perf
 
-    # Batch length of 20
-    # Seed 1
+    # ## Batch length of 20
+    # ## Seed 1
     # export CUDA_VISIBLE_DEVICES=1
     # (sleep 1s && python dreamer_atari.py \
     #     --track --capture-video \
-    #     --env-id "PongNoFrameskip-v4" \
+    #     --env-id "BreakoutNoFrameskip-v4" \
     #     --total-timesteps 10000000 \
     #     --batch-size 50 --batch-length 20 \
     #     --train-every 16 \
@@ -48,7 +48,7 @@ export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
     # export CUDA_VISIBLE_DEVICES=0
     # (sleep 1s && python dreamer_atari.py \
     #     --track --capture-video \
-    #     --env-id "PongNoFrameskip-v4" \
+    #     --env-id "BreakoutNoFrameskip-v4" \
     #     --total-timesteps 10000000 \
     #     --batch-size 50 --batch-length 20 \
     #     --train-every 16 \
