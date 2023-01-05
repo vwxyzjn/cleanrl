94a44b5a252c432e3c47577fa46ed49c230fcce3
Costa Huang <costa.huang@outlook.com>
2023-01-04 21:12:52 -0500

    Hotfix for #331 (#342)

--- tests/test_mujoco.py
+++ tests/test_mujoco.py
@@ -15,3 +15,13 @@ def test_mujoco():
         shell=True,
         check=True,
     )
+    subprocess.run(
+        "python cleanrl/rpo_continuous_action.py --env-id Hopper-v4 --num-envs 1 --num-steps 64 --total-timesteps 128",
+        shell=True,
+        check=True,
+    )
+    subprocess.run(
+        "python cleanrl/rpo_continuous_action.py --env-id dm_control/cartpole-balance-v0 --num-envs 1 --num-steps 64 --total-timesteps 128",
+        shell=True,
+        check=True,
+    )
