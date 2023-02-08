4e93341a2fd487a342a49d8b9181a101cea08f3f
Costa Huang <costa.huang@outlook.com>
2023-02-08 19:08:48 +0000

    workaround for sail-sg/envpool#239

--- cleanrl_utils/evals/ppo_envpool_jax_eval.py
+++ cleanrl_utils/evals/ppo_envpool_jax_eval.py
@@ -58,28 +58,26 @@ def evaluate(
     for episode in range(eval_episodes):
         episodic_return = 0
         next_obs = envs.reset()
-        done = False
-
         if capture_video:
             recorded_frames = []
             # conversion from grayscale into rgb
             recorded_frames.append(cv2.cvtColor(next_obs[0][-1], cv2.COLOR_GRAY2RGB))
-        while not done:
+        for _ in range(envs.spec.config.max_episode_steps):
             actions, key = get_action_and_value(network_params, actor_params, next_obs, key)
             next_obs, _, _, infos = envs.step(np.array(actions))
             episodic_return += infos["reward"][0]
             done = sum(infos["terminated"]) + sum(infos["TimeLimit.truncated"]) >= 1
-
             if capture_video and episode == 0:
                 recorded_frames.append(cv2.cvtColor(next_obs[0][-1], cv2.COLOR_GRAY2RGB))
-
             if done:
-                print(f"eval_episode={len(episodic_returns)}, episodic_return={episodic_return}")
-                episodic_returns.append(episodic_return)
-                if capture_video and episode == 0:
-                    clip = ImageSequenceClip(recorded_frames, fps=24)
-                    os.makedirs(f"videos/{run_name}", exist_ok=True)
-                    clip.write_videofile(f"videos/{run_name}/{episode}.mp4", logger="bar")
+                break
+
+        print(f"eval_episode={len(episodic_returns)}, episodic_return={episodic_return}")
+        episodic_returns.append(episodic_return)
+        if capture_video and episode == 0:
+            clip = ImageSequenceClip(recorded_frames, fps=24)
+            os.makedirs(f"videos/{run_name}", exist_ok=True)
+            clip.write_videofile(f"videos/{run_name}/{episode}.mp4", logger="bar")
 
     return episodic_returns
 
