599f9adfec89d63721578b08b75ec38ab0209372
Valentin Canete <valentin.canete@gmail.com>
2023-04-09 11:24:03 +0200

    fix make_env for rendering

--- cleanrl/dqn_atari_jax.py
+++ cleanrl/dqn_atari_jax.py
@@ -88,11 +88,12 @@ def parse_args():
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
-        env = gym.make(env_id)
+        if capture_video and idx == 0:
+            env = gym.make(env_id, render_mode="rgb_array")
+            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        else:
+            env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
-        if capture_video:
-            if idx == 0:
-                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         env = NoopResetEnv(env, noop_max=30)
         env = MaxAndSkipEnv(env, skip=4)
         env = EpisodicLifeEnv(env)
@@ -103,6 +104,7 @@ def make_env(env_id, seed, idx, capture_video, run_name):
         env = gym.wrappers.GrayScaleObservation(env)
         env = gym.wrappers.FrameStack(env, 4)
         env.action_space.seed(seed)
+
         return env
 
     return thunk
