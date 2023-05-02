60200d258a8355426b28d3a76d4722581d1b887f
Costa Huang <costa.huang@outlook.com>
2023-05-02 09:43:49 -0400

    add warning mesage and update dependencies

--- cleanrl/dqn_atari_jax.py
+++ cleanrl/dqn_atari_jax.py
@@ -141,6 +141,15 @@ def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
 
 
 if __name__ == "__main__":
+    import stable_baselines3 as sb3
+
+    if sb3.__version__ < "2.0":
+        raise ValueError(
+            """Ongoing migration: run the following command to install the new dependencies:
+
+poetry run pip install "stable_baselines3==2.0.0a1" "gymnasium[atari,accept-rom-license]==0.28.1"  "ale-py==0.8.1" 
+"""
+        )
     args = parse_args()
     run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
     if args.track:
