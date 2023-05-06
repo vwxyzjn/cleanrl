34f68ed5efef1cf24105c802c7f2f46dd19381ec
Adam Zhao <pazyx728@gmail.com>
2023-05-07 05:41:53 +0800

    add eval model test cases

--- tests/test_atari_jax.py
+++ tests/test_atari_jax.py
@@ -1,9 +1,17 @@
 import subprocess
 
 
-def test_dqn_jax():
+def test_c51_jax():
     subprocess.run(
-        "python cleanrl/dqn_atari_jax.py --learning-starts 10 --total-timesteps 16 --buffer-size 10 --batch-size 4",
+        "python cleanrl/c51_atari_jax.py --learning-starts 10 --total-timesteps 16 --buffer-size 10 --batch-size 4",
+        shell=True,
+        check=True,
+    )
+
+
+def test_c51_jax_eval():
+    subprocess.run(
+        "python cleanrl/c51_atari_jax.py --save-model True --learning-starts 10 --total-timesteps 16 --buffer-size 10 --batch-size 4",
         shell=True,
         check=True,
     )
