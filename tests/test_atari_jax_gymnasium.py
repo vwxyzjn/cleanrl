3a766b3beff190a9e87b3f6022d7fbc4418e13a8
Adam Zhao <pazyx728@gmail.com>
2023-05-19 15:25:20 +0800

    add tests

--- tests/test_atari_jax_gymnasium.py
+++ tests/test_atari_jax_gymnasium.py
@@ -23,3 +23,11 @@ def test_qdagger_dqn_atari_jax_impalacnn():
         shell=True,
         check=True,
     )
+
+
+def test_qdagger_dqn_atari_jax_impalacnn_eval():
+    subprocess.run(
+        "python cleanrl/qdagger_dqn_atari_jax_impalacnn.py --save-model True --learning-starts 10 --total-timesteps 16 --buffer-size 10 --batch-size 4 --teacher-steps 16 --offline-steps 16 --teacher-eval-episodes 1",
+        shell=True,
+        check=True,
+    )
