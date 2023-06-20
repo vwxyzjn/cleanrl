73538d175ed905d365dcff3f678067db203c76eb
Adam Zhao <pazyx728@gmail.com>
2023-06-20 20:39:25 +0800

    update c51 & c51_jax tests

--- tests/test_classic_control_gymnasium.py
+++ tests/test_classic_control_gymnasium.py
@@ -7,3 +7,19 @@ def test_dqn():
         shell=True,
         check=True,
     )
+
+
+def test_c51():
+    subprocess.run(
+        "python cleanrl/c51.py --learning-starts 200 --total-timesteps 205",
+        shell=True,
+        check=True,
+    )
+
+
+def test_c51_eval():
+    subprocess.run(
+        "python cleanrl/c51.py --save-model True --learning-starts 200 --total-timesteps 205",
+        shell=True,
+        check=True,
+    )
