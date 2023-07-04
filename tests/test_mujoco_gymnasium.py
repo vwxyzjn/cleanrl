f62ad1f942890db7b752f4ad79b65be53917d3c2
Adam(Yanxiao) Zhao <pazyx728@gmail.com>
2023-07-04 12:48:31 +0800

    ddpg huggingface integration (#407)
    * add huggingface integration to ddpg
    
    * ddpg_jax huggingface integration
    
    * update docs
    
    * update tests
    
    * fix type-hint in py38
    
    * fix type-hint in py38
    
    * try fix
    
    * retry
    
    * reretry
    
    * rereretry
    
    * deprecated macos mujoco env tests

--- tests/test_mujoco_gymnasium.py
+++ tests/test_mujoco_gymnasium.py
@@ -15,3 +15,19 @@ def test_mujoco():
         shell=True,
         check=True,
     )
+
+
+def test_mujoco_eval():
+    """
+    Test mujoco_eval
+    """
+    subprocess.run(
+        "python cleanrl/ddpg_continuous_action.py --save-model True --env-id Hopper-v4 --learning-starts 100 --batch-size 32 --total-timesteps 105",
+        shell=True,
+        check=True,
+    )
+    subprocess.run(
+        "python cleanrl/ddpg_continuous_action_jax.py --save-model True --env-id Hopper-v4 --learning-starts 100 --batch-size 32 --total-timesteps 105",
+        shell=True,
+        check=True,
+    )
