0fceeefbd99fa23abd15f48dbd984d7eb0b124ee
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2023-10-04 15:15:56 +0900

    Fixed incorrect actor_loss shape for SAC continuous, addresses issue #379 (#383)

--- cleanrl/sac_continuous_action.py
+++ cleanrl/sac_continuous_action.py
@@ -266,7 +266,7 @@ if __name__ == "__main__":
                     pi, log_pi, _ = actor.get_action(data.observations)
                     qf1_pi = qf1(data.observations, pi)
                     qf2_pi = qf2(data.observations, pi)
-                    min_qf_pi = torch.min(qf1_pi, qf2_pi).view(-1)
+                    min_qf_pi = torch.min(qf1_pi, qf2_pi)
                     actor_loss = ((alpha * log_pi) - min_qf_pi).mean()
 
                     actor_optimizer.zero_grad()
