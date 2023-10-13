73d3c10ba1d82a0ca5ff33a1f26f15526b4bf3bb
Adam Zhao <pazyx728@gmail.com>
2023-10-13 21:41:36 +0800

    test_pybullet.py

--- tests/test_pybullet.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def test_pybullet():
-    subprocess.run(
-        "python cleanrl/td3_continuous_action.py --learning-starts 100 --batch-size 32 --total-timesteps 105",
-        shell=True,
-        check=True,
-    )
