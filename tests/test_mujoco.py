8e8dd818ed5958888899f0c0537f518280ae35b0
Adam Zhao <pazyx728@gmail.com>
2023-10-13 20:52:17 +0800

    fix tests scripts

--- tests/test_mujoco.py
+++ tests/test_mujoco.py
@@ -16,12 +16,12 @@ def test_mujoco():
         check=True,
     )
     subprocess.run(
-        "python cleanrl/td3_continuous_action.py ---env-id Hopper-v4 -learning-starts 100 --batch-size 32 --total-timesteps 105",
+        "python cleanrl/td3_continuous_action.py --env-id Hopper-v4 --learning-starts 100 --batch-size 32 --total-timesteps 105",
         shell=True,
         check=True,
     )
     subprocess.run(
-        "python cleanrl/td3_continuous_action_jax.py ---env-id Hopper-v4 -learning-starts 100 --batch-size 32 --total-timesteps 105",
+        "python cleanrl/td3_continuous_action_jax.py --env-id Hopper-v4 --learning-starts 100 --batch-size 32 --total-timesteps 105",
         shell=True,
         check=True,
     )
