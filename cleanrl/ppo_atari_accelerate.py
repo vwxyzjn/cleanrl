26dd0e3df4c25d39e73a3cc2c235e298f1bc8874
Edward Beeching <edbeeching@users.noreply.github.com>
2024-02-13 10:09:00 +0100

    Update cleanrl/ppo_atari_accelerate.py
    Co-authored-by: Costa Huang <costa.huang@outlook.com>

--- cleanrl/ppo_atari_accelerate.py
+++ cleanrl/ppo_atari_accelerate.py
@@ -209,12 +209,12 @@ if __name__ == "__main__":
     device = accelerator.device
 
     # ALGO Logic: Storage setup
-    obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
-    actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
-    logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    values = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    obs = torch.zeros((args.num_steps, args.local_num_envs) + envs.single_observation_space.shape).to(device)
+    actions = torch.zeros((args.num_steps, args.local_num_envs) + envs.single_action_space.shape).to(device)
+    logprobs = torch.zeros((args.num_steps, args.local_num_envs)).to(device)
+    rewards = torch.zeros((args.num_steps, args.local_num_envs)).to(device)
+    dones = torch.zeros((args.num_steps, args.local_num_envs)).to(device)
+    values = torch.zeros((args.num_steps, args.local_num_envs)).to(device)
 
     # TRY NOT TO MODIFY: start the game
     global_step = 0
