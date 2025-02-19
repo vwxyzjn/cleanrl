5455fd170b77c9a95e9470e062ec4e00f5d04eca
roger-creus <creus99@gmail.com>
2025-02-19 11:15:47 -0500

    fix poetry command in benchmark script

--- benchmark/pqn.sh
+++ benchmark/pqn.sh
@@ -1,3 +1,4 @@
+poetry install
 OMP_NUM_THREADS=1 xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
     --env-ids CartPole-v1 Acrobot-v1 MountainCar-v0 \
     --command "poetry run python cleanrl/pqn.py --no_cuda --track" \
@@ -8,6 +9,7 @@ OMP_NUM_THREADS=1 xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
     --slurm-total-cpus 10 \
     --slurm-template-path benchmark/cleanrl_1gpu.slurm_template
 
+poetry install -E envpool
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids Breakout-v5 SpaceInvaders-v5 BeamRider-v5 Pong-v5 MsPacman-v5 \
     --command "poetry run python cleanrl/pqn_atari_envpool.py --track" \
@@ -18,6 +20,7 @@ poetry run python -m cleanrl_utils.benchmark \
     --slurm-total-cpus 10 \
     --slurm-template-path benchmark/cleanrl_1gpu.slurm_template
 
+poetry install -E envpool
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids Breakout-v5 SpaceInvaders-v5 BeamRider-v5 Pong-v5 MsPacman-v5 \
     --command "poetry run python cleanrl/pqn_atari_envpool_lstm.py --track" \
