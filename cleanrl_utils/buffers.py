6587ee6f1bea1c2ae253602b34b1d562c4ea6809
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2025-03-19 14:39:41 +0000

    Add `get_obs_shape` function

--- cleanrl_utils/buffers.py
+++ cleanrl_utils/buffers.py
@@ -67,6 +67,33 @@ def get_action_dim(action_space: spaces.Space) -> int:
         raise NotImplementedError(f"{action_space} action space is not supported")
 
 
+def get_obs_shape(
+    observation_space: spaces.Space,
+) -> tuple[int, ...] | dict[str, tuple[int, ...]]:
+    """
+    Get the shape of the observation (useful for the buffers).
+
+    :param observation_space:
+    :return:
+    """
+    if isinstance(observation_space, spaces.Box):
+        return observation_space.shape
+    elif isinstance(observation_space, spaces.Discrete):
+        # Observation is an int
+        return (1,)
+    elif isinstance(observation_space, spaces.MultiDiscrete):
+        # Number of discrete features
+        return (int(len(observation_space.nvec)),)
+    elif isinstance(observation_space, spaces.MultiBinary):
+        # Number of binary features
+        return observation_space.shape
+    elif isinstance(observation_space, spaces.Dict):
+        return {key: get_obs_shape(subspace) for (key, subspace) in observation_space.spaces.items()}  # type: ignore[misc]
+
+    else:
+        raise NotImplementedError(f"{observation_space} observation space is not supported")
+
+
 def get_device(device: th.device | str = "auto") -> th.device:
     """
     Retrieve PyTorch device.
